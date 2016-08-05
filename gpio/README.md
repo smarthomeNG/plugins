6569dbd3d6f784dfb58d097ba0de282a5e64b481
onkelandy <onkelandy@hotmail.com>
2016-08-05 13:46:55 +0200

    GPIO Smart Plugin
    Made it a smart plugin

---
+++ gpio/README.md
@@ -0,0 +1,56 @@
+# GPIO
+
+# Requirements
+
+RPi.GPIO Python module
+
+Install it with:
+sudo pip3 install RPi.GPIO --upgrade
+
+## Supported Hardware
+
+Raspberry Pi all versions (tested on Raspberry Pi 1 revision 2)
+
+# Configuration
+
+## plugin.conf
+
+
+<pre>
+[GPIO]
+   class_name = Raspi_GPIO
+   class_path = plugins.gpio
+#   cycle = 0
+#   mode = BOARD
+</pre>
+
+Attributes:
+cycle: Integer. Defines the time in seconds until the next evaluation of the GPIO pins. If not set, default value is 0. cycle = 0 means that the pins are constantly read and written.
+mode: Define the GPIO PIN Mode that you use to declare the pin numbers. If not set, default is BOARD
+Possible modes: BOARD or BCM. More information can be found here: 
+http://raspberrypi.stackexchange.com/questions/12966/what-is-the-difference-between-board-and-bcm-for-gpio-pin-numbering
+
+
+## items.conf
+
+### gpio_in
+
+Define the pin number of your Raspberry Pi that should be read, i.e. where a sensor is attached. Beware that the number has to follow the rules of the "mode" you have defined in the plugin.conf (Board or BCM).
+
+### gpio_out
+
+Define the pin number of your Raspberry Pi that should be written, i.e. where a LED is attached. Beware that the number has to follow the rules of the "mode" you have defined in the plugin.conf (Board or BCM). The Output-Pin will also automatically be read by the plugin as Input.
+
+### Example
+
+<pre>
+# items/my.conf
+[item1]
+    type = bool
+    visu_acl = ro
+    gpio_in = 10
+[item2]
+    type = bool
+    visu_acl = rw
+    gpio_out = 13
+</pre>
--- gpio/__init__.py
+++ gpio/__init__.py
@@ -1,120 +1,157 @@
 #!/usr/bin/env python3
 # vim: set encoding=utf-8 tabstop=4 softtabstop=4 shiftwidth=4 expandtab
 #########################################################################
-#  Onkel Andy
+#  Copyright 2016 <Onkel Andy>                    <onkelandy@hotmail.com>
 #########################################################################
-#  GPIO plugin for SmartHome.py http://mknx.github.com/smarthome/
+#  This file is part of SmartHomeNG.   
 #
-#  This plugin is free software: you can redistribute it and/or modify
+#  SmartHomeNG is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
-#  This plugin is distributed in the hope that it will be useful,
+#  SmartHomeNG is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with this plugin. If not, see <http://www.gnu.org/licenses/>.
+#  along with SmartHomeNG. If not, see <http://www.gnu.org/licenses/>.
 #########################################################################
 
-import datetime
-import RPi.GPIO as GPIO
 import logging
+from lib.model.smartplugin import SmartPlugin
+import RPi.GPIO as GPIO
 import threading
 import time
 
-logger = logging.getLogger('GPIO')
+class Raspi_GPIO(SmartPlugin):
+    PLUGIN_VERSION = "1.0.0"
+    ALLOW_MULTIINSTANCE = False
 
-class Raspi_GPIO(object):
-    _items = []
-    
-    def wait(self,time_lapse):
-    	time_start = time.time()
-    	time_end = (time_start + time_lapse)
+    def _wait(self,time_lapse):
+        time_start = time.time()
+        time_end = (time_start + time_lapse)
      
-    	while time_end > time.time():
-    		pass
-    
-    def __init__(self,smarthome,cycle):
-        self._sh = smarthome
+        while time_end > time.time():
+            pass
+
+    def __init__(self, sh, cycle=0, mode="board"):
+        self.logger = logging.getLogger(__name__)
+        self._sh = sh
+        self._items = []
         self._cycle = int(cycle)
-        self.alive = 'False'
-        self._lock = threading.Lock()
-        GPIO.setmode(GPIO.BOARD)
-     
+        self._mode = mode.upper()
+        GPIO.setwarnings(False)
+        if self._mode == "BCM":
+            GPIO.setmode(GPIO.BCM)
+        else:
+            GPIO.setmode(GPIO.BOARD)    
+        self.logger.debug("GPIO: Mode set to {0}".format(self._mode))
+        self.alive = False
+        self._lock = threading.Lock() 
+        
+
     def run(self):
-      self.alive = True
-      if self._cycle > 0:
+        self.logger.debug("run method called")
+        self.alive = True          
+        if self._cycle > 0:
             self._sh.scheduler.add('Raspi_GPIO', self.get_sensors, prio=5, cycle=self._cycle)  
-      else:
+        else:
+            self.logger.debug("GPIO: No Cycle defined, just running all the time.")
             self.get_sensors() 
-            logger.debug("GPIO: No Cycle defined")
             
+
     def stop(self):
         self.alive = False
-        GPIO.cleanup()
         try:
             self._sh.scheduler.remove('Raspi_GPIO')
         except:
-            logger.error("GPIO: Removing of scheduler failed: {}".format(sys.exc_info()))
-                
+            self.logger.error("GPIO: Removing of scheduler failed: {}".format(sys.exc_info()))
+        self._wait(2)
+        GPIO.cleanup()
+        
+
+    def parse_item(self, item):
+        if self.has_iattr(item.conf, 'gpio_in'):
+            in_pin = int(item.conf['gpio_in'])
+            GPIO.setup(in_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            self.logger.debug("GPIO INPUT: {0} assigned to pin \'{1}\'".format(item, in_pin))
+            self._items.append(item)
+            return self.update_item      
+        if self.has_iattr(item.conf, 'gpio_out'):
+            out_pin = int(item.conf['gpio_out'])
+            GPIO.setup(out_pin, GPIO.OUT)
+            self.logger.debug("GPIO OUTPUT: {0} assigned to \'{1}\'".format(item, out_pin))
+            if (out_pin is None):
+                return None
+                self.logger.debug("GPIO: No out_pin set")
+            else:
+                self._items.append(item)
+                self.logger.debug("item: {0}".format(item))
+            return self.update_item
+
+    def parse_logic(self, logic):
+        pass
+
+    def update_item(self, item, caller=None, source=None, dest=None):
+        if item():
+            if self.has_iattr(item.conf, 'gpio_out'):
+                out_pin = int(item.conf['gpio_out'])
+                value = item()                   
+                self.logger.debug("GPIO OUTPUT: Setting pin {0} ({2}) to {1}.".format(out_pin, value, item.id()))
+                self.send(out_pin, value)
+            else:
+                self.logger.debug("GPIO: No gpio_out")
+
+    def get_sensors(self):   
+        if self._cycle > 0:
+            try:
+                for item in self._items:
+                    if 'gpio_in' in item.conf:
+                        sensor = int(item.conf['gpio_in']) 
+                        value = GPIO.input(sensor)
+                        item(value, 'GPIO', 'get_sensors')
+                        self.logger.debug("GPIO SENSOR: {0}  VALUE: {1}".format(sensor,value))
+                        self._wait(0.1)
+                    if 'gpio_out' in item.conf:
+                        sensor = int(item.conf['gpio_out']) 
+                        value = GPIO.input(sensor)
+                        item(value, 'GPIO', 'get_sensors')
+                        self.logger.debug("GPIO OUTPUT: {0}  VALUE: {1}".format(sensor,value))
+                        self._wait(0.1)
+            except Exception as e:
+                self.logger.warning("GPIO ERROR: {0}".format(e))
+        else:
+            while self.alive:
+                try:
+                    for item in self._items:
+                        if 'gpio_in' in item.conf:
+                            sensor = int(item.conf['gpio_in']) 
+                            value = GPIO.input(sensor)
+                            item(value, 'GPIO', 'get_sensors')
+                            self.logger.debug("GPIO SENSOR: {0}  VALUE: {1}".format(sensor,value))
+                            self._wait(0.1)
+                        if 'gpio_out' in item.conf:
+                            sensor = int(item.conf['gpio_out']) 
+                            value = GPIO.input(sensor)
+                            item(value, 'GPIO', 'get_sensors')
+                            self.logger.debug("GPIO OUTPUT: {0}  VALUE: {1}".format(sensor,value))
+                            self._wait(0.1)
+                except Exception as e:
+                    self.logger.warning("GPIO ERROR: {0}".format(e))
+
     def send(self, pin, value):
         self._lock.acquire()
         try:
             GPIO.output(pin, value)
-            logger.info("GPIO: Pin {0} successfully set to {1}".format(pin, value))
+            self.logger.info("GPIO: Pin {0} successfully set to {1}".format(pin, value))
         except:
-            logger.error("GPIO: Send {0} failed for {1}!".format(value, pin))
+            self.logger.error("GPIO: Send {0} failed for {1}!".format(value, pin))
         finally:
             self._lock.release()                
-        
-    def parse_item(self, item):
-        if 'gpio_in' in item.conf:
-            in_pin = int(item.conf['gpio_in'])
-            GPIO.setup(in_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-            logger.debug("GPIO INPUT: {0} assigned to pin \'{1}\'".format(item, in_pin))
-            self._items.append(item)
-            return self.update_item      
-        if 'gpio_out' in item.conf:
-            out_pin = int(item.conf['gpio_out'])
-            GPIO.setup(out_pin, GPIO.OUT)
-            logger.debug("GPIO OUTPUT: {0} assigned to \'{1}\'".format(item, out_pin))
-            if (out_pin is None):
-                return None
-                logger.debug("GPIO: No out_pin set")
-            else:
-                self._items.append(item)
-            return self.update_item
-            
-    def update_item(self, item, caller=None, source=None, dest=None):
-        if caller != 'Raspi_GPIO':
-            #if item():
-                if 'gpio_out' in item.conf:
-                    out_pin = int(item.conf['gpio_out'])
-                    value = item()                   
-                    logger.debug("GPIO OUTPUT: Setting pin {0} ({2}) to {1}.".format(out_pin, value, item.id()))
-                    self.send(out_pin, value)
-                else:
-                    logger.debug("GPIO: No gpio_out")
-               
-    def get_sensors(self):
-     try:         
-           for item in self._items:
-               if 'gpio_in' in item.conf:
-                   sensor = int(item.conf['gpio_in']) 
-                   value = GPIO.input(sensor)
-                   item(value, 'GPIO', 'get_sensors')
-                   logger.debug("GPIO SENSOR: {0}  VALUE: {1}".format(sensor,value))
-                   self.wait(0.2)
-               if 'gpio_out' in item.conf:
-                   sensor = int(item.conf['gpio_out']) 
-                   value = GPIO.input(sensor)
-                   item(value, 'GPIO', 'get_sensors')
-                   logger.debug("GPIO OUTPUT: {0}  VALUE: {1}".format(sensor,value))
-                   self.wait(0.2)
 
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG, format='%(relativeCreated)6d %(threadName)s %(message)s')
+    FooClass(None).run()
 
-     except Exception as e:
-      logger.warning("GPIO ERROR: {0}".format(e))
