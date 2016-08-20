215414d763e70abffd141594699f94fed1928ea2
Oliver Hinckel <info@ollisnet.de>
2016-08-20 21:30:42 +0200

    - integrate enable/disable logics feature from visu plugin into visu_websocket

--- visu_websocket/__init__.py
+++ visu_websocket/__init__.py
@@ -342,10 +342,18 @@ class websockethandler(lib.connection.Stream):
             if 'name' not in data or 'val' not in data:
                 return
             name = data['name']
-            value = data['val']
             if name in self.logics:
-                self.logger.info("Client {0} triggerd logic {1} with '{2}'".format(self.addr, name, value))
-                self.logics[name].trigger(by='Visu', value=value, source=self.addr)
+                if 'val' in data:
+                    value = data['val']
+                    self.logger.info("Client {0} triggerd logic {1} with '{2}'".format(self.addr, name, value))
+                    self.logics[name].trigger(by='Visu', value=value, source=self.addr)
+                if 'enabled' in data:
+                    if data['enabled']:
+                        self.logger.info("Client {0} enabled logic {1}".format(self.addr, name))
+                        self.logics[name].enable()
+                    else:
+                        self.logger.info("Client {0} disabled logic {1}".format(self.addr, name))
+                        self.logics[name].disable()
             else:
 #                self.logger.warning("VISU: Defined logics {0}".format(self.logics))
                 self.logger.warning("Client {0} requested invalid logic: {1}".format(self.addr, name))
