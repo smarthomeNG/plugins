128f3896e94fe5ae2c781c9c8dbc769930f82cfb
Marc René Frieß <rene.friess@gmx.de>
2016-08-21 08:59:00 +0200

    Backend: fixes issues with html code in items / scheduler. so far items with html in the str value cannot be changed via backend plugin. i can't get the processing when sending the value work

---
+++ backend/static/js/backend.js
@@ -0,0 +1,17 @@
+function htmlEscape(str) {
+    return str
+        .replace(/&/g, '&amp;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;');
+}
+
+function htmlUnescape(str){
+    return str
+        .replace(/&quot;/g, '"')
+        .replace(/&#39;/g, "'")
+        .replace(/&lt;/g, '<')
+        .replace(/&gt;/g, '>')
+        .replace(/&amp;/g, '&');
+}
