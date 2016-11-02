201a8544a27a6e8b17efb3c61dc7d8eb753d4464
Marc René Frieß <rene.friess@gmx.de>
2016-11-02 19:53:57 +0100

    knx: optimized check for missing knx_dpt

--- knx/__init__.py
+++ knx/__init__.py
@@ -241,8 +241,7 @@ class KNX(lib.connection.Client):
             if dpt not in dpts.decode:
                 self.logger.warning("KNX[{0}]: Ignoring {1} unknown dpt: {2}".format(self.instance, item, dpt))
                 return None
-        elif 'knx_dpt' not in item.conf and (
-                                'knx_status' in item.conf or 'knx_send' in item.conf or 'knx_reply' in item.conf or 'knx_listen' in item.conf or 'knx_init' in item.conf or 'knx_cache' in item.conf):
+        elif 'knx_status' in item.conf or 'knx_send' in item.conf or 'knx_reply' in item.conf or 'knx_listen' in item.conf or 'knx_init' in item.conf or 'knx_cache' in item.conf:
             self.logger.warning(
                 "KNX[{0}]: Ignoring {1}: please add knx_dpt.".format(self.instance, item))
             return None
