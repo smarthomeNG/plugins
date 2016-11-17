7337bbab4423dae282d936b3e837f1f364dedd38
adamu <bakowski.a@gmail.com>
2016-11-17 20:22:19 +0100

    systemair: make it smarter

--- systemair/__init__.py
+++ systemair/__init__.py
@@ -27,14 +27,13 @@ import threading
 from ctypes import c_short
 from lib.model.smartplugin import SmartPlugin
 
-logger = logging.getLogger('Systemair')
-
 class Systemair(SmartPlugin):
     PLUGIN_VERSION = "1.3.0.1"
     ALLOW_MULTIINSTANCE = False
 
     def __init__(self, smarthome, serialport, slave_address="1", update_cycle="30"):
         self._sh = smarthome
+        self.logger = logging.getLogger(__name__)
         self.instrument = None
         self.slave_address = int(slave_address)
         self._update_coil = {}
@@ -71,7 +70,7 @@ class Systemair(SmartPlugin):
             return True
         except:
             self.instrument = None
-            logger.error("Failed to initialize modbus on {serialport}".format(serialport=serialport))
+            self.logger.error("Failed to initialize modbus on {serialport}".format(serialport=serialport))
             return False
 
     def _read_modbus(self):
@@ -97,7 +96,7 @@ class Systemair(SmartPlugin):
                             try:
                                 item(read_regs[reg], 'systemair_value_from_bus', "Reg {}".format(reg))
                             except Exception as e:
-                                logger.error("Modbus: Exception when updating {} {}".format(item, e))
+                                self.logger.error("Modbus: Exception when updating {} {}".format(item, e))
 
             # get coils
             for coil_addr in self._update_coil:
@@ -107,7 +106,7 @@ class Systemair(SmartPlugin):
                         item(value, 'systemair_value_from_bus', "Coil {}".format(coil_addr))
 
         except Exception as err:
-            logger.error(err)
+            self.logger.error(err)
             self.instrument = None
         finally:
             self._lockmb.release()
@@ -120,7 +119,7 @@ class Systemair(SmartPlugin):
         self.alive = False
 
     def connect(self):
-        logger.debug("systemair_value_from_bus: connect")
+        self.logger.debug("systemair_value_from_bus: connect")
 
     def update_item(self, item, caller=None, source=None, dest=None):
         # ignore values from bus
@@ -134,7 +133,7 @@ class Systemair(SmartPlugin):
     def parse_item(self, item):
         if self.has_iattr(item.conf, 'systemair_regaddr'):
             modbus_regaddr = int(self.get_iattr_value(item.conf, 'systemair_regaddr'))
-            logger.debug("systemair_value_from_bus: {0} connected to register {1:#04x}".format(item, modbus_regaddr))
+            self.logger.debug("systemair_value_from_bus: {0} connected to register {1:#04x}".format(item, modbus_regaddr))
             self.my_reg_items.append(item)
             for reg_set in self._reg_sets:
                 if modbus_regaddr in reg_set['range']:
@@ -149,13 +148,13 @@ class Systemair(SmartPlugin):
                         reg_set['range_used'] = range(min(reg_set['range_used'].start, modbus_regaddr), max(reg_set['range_used'].stop, modbus_regaddr + 1))
                     else:
                         reg_set['range_used'] = range(modbus_regaddr, modbus_regaddr + 1)
-                    logger.debug("systemair: adding reg {} to reg_set {} {}".format(modbus_regaddr, reg_set['name'], reg_set['range_used']))
-                    logger.debug("systemair: regs used: {}".format(reg_set['regs_used']))
+                    self.logger.debug("systemair: adding reg {} to reg_set {} {}".format(modbus_regaddr, reg_set['name'], reg_set['range_used']))
+                    self.logger.debug("systemair: regs used: {}".format(reg_set['regs_used']))
                     break
 
         if self.has_iattr(item.conf, 'systemair_coiladdr'):
             modbus_coiladdr = int(self.get_iattr_value(item.conf, 'systemair_coiladdr'))
-            logger.debug("systemair_value_from_bus: {0} connected to coil register {1:#04x}".format(item, modbus_coiladdr))
+            self.logger.debug("systemair_value_from_bus: {0} connected to coil register {1:#04x}".format(item, modbus_coiladdr))
             if not modbus_coiladdr in self._update_coil:
                 self._update_coil[modbus_coiladdr] = set()
 
@@ -165,9 +164,9 @@ class Systemair(SmartPlugin):
 
     def _write_register_value(self, item, repeat_count=0):
         try:
-            logger.debug('writing register value')
+            self.logger.debug('writing register value')
             if not self.has_iattr(item.conf, 'systemair_regaddr'):
-                logger.error('Could not write to modbus. Register address missing!')
+                self.logger.error('Could not write to modbus. Register address missing!')
                 return
             # BUG in Systemair docu, register starts with 100, not 101
             reg_addr = int(self.get_iattr_value(item.conf, 'systemair_regaddr')) - 1
@@ -177,14 +176,14 @@ class Systemair(SmartPlugin):
         except serial.serialutil.SerialException as err:
             if err.args[0].lower() == 'port is already open.':
                 if self.mod_write_repeat < repeat_count:
-                    logger.error("Could not write register value to systemair modbus. Maximal retries reached.")
+                    self.logger.error("Could not write register value to systemair modbus. Maximal retries reached.")
                     return
                 repeat_count += 1
-                logger.warning('Could not write register value to systemair modbus. Port is already open.')
-                logger.warning('Repeat {rep} of {max}.'.format(rep=repeat_count, max=self.mod_write_repeat))
+                self.logger.warning('Could not write register value to systemair modbus. Port is already open.')
+                self.logger.warning('Repeat {rep} of {max}.'.format(rep=repeat_count, max=self.mod_write_repeat))
                 sleep(1)
                 self._write_register_value(item, repeat_count)
             pass
         except Exception as err:
-            logger.error('Could not write register value to modbus. Error: {err}!'.format(err=err))
+            self.logger.error('Could not write register value to modbus. Error: {err}!'.format(err=err))
             self.instrument = None
