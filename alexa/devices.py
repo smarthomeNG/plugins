2c93c42d27b81c24d559f829cc6fe33621065400
Kai Meder <kai.meder@eurofit24.com>
2016-11-28 15:03:14 +0100

    forgot self arguments

--- alexa/devices.py
+++ alexa/devices.py
@@ -2,14 +2,14 @@ class AlexaDevices(object):
     def __init__(self):
         self.devices = {}
 
-    def exists(id):
+    def exists(self, id):
         return id in self.devices
 
-    def get(id):
+    def get(self, id):
         return self.devices[id]
 
-    def put(device):
+    def put(self, device):
         self.devices[device.id] = device
 
-    def all():
+    def all(self):
         return self.devices.values()
