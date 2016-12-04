f9a714230894f464e79c56052ccdf6889760f761
Kai Meder <kai.meder@eurofit24.com>
2016-12-04 01:13:16 +0100

    explicit utf-8

--- alexa/service.py
+++ alexa/service.py
@@ -72,7 +72,7 @@ class AlexaRequestHandler(BaseHTTPRequestHandler):
     def respond(self, response):
         data = json.dumps(response).encode('utf-8')
         self.send_response(200)
-        self.send_header('Content-Type', 'application/json')
+        self.send_header('Content-Type', 'application/json; charset=utf-8')
         self.send_header('Content-Length', len(data))
         self.end_headers()
         self.wfile.write(data)
