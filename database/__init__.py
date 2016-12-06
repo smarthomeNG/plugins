e92cdf3ea9a4923cdd873cdf68372f8867de5941
ohinckel <ohinckel@users.noreply.github.com>
2016-12-06 07:55:23 +0100

    - fix licence header: remove cstrassburg, add ohinckel

--- database/__init__.py
+++ database/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # vim: set encoding=utf-8 tabstop=4 softtabstop=4 shiftwidth=4 expandtab
 #########################################################################
-# Copyright 2016-     Christian Strassburg            c.strassburg@gmx.de
+# Copyright 2016-     Oliver Hinckel                   github@ollisnet.de
 #########################################################################
 #  This file is part of SmartHomeNG
 #  https://github.com/smarthomeNG/smarthome
