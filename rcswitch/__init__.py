6af38a0e874607f05dbf768bbec69098b9d65822
dasax <danielfrank@gmx.net>
2017-02-14 13:07:44 +0100

    modified:   rcswitch/__init__.py

--- rcswitch/__init__.py
+++ rcswitch/__init__.py
@@ -19,43 +19,67 @@
 #  along with SmartHomeNG. If not, see <http://www.gnu.org/licenses/>.
 #########################################################################
 
+import os
 import logging
 from lib.model.smartplugin import SmartPlugin
 from datetime import datetime, timedelta
-import os
 import time
 import threading
+import subprocess
+from subprocess import DEVNULL
+import shlex
 
 class RCswitch(SmartPlugin):
 
 	ALLOW_MULTIINSTANCE = False
-	PLUGIN_VERSION = "1.2.0.1"
+	PLUGIN_VERSION = "1.2.0.2"
 
-	def __init__(self, smarthome, rcswitch_dir='/usr/local/bin/rcswitch-pi', rcswitch_sendDuration='0.5'):
+	def __init__(self, smarthome, rcswitch_dir='/usr/local/bin/rcswitch-pi', rcswitch_sendDuration='0.5', rcswitch_host='', rcswitch_user='', rcswitch_password=''):
 		self.logger = logging.getLogger(__name__)
-		self.logger.info('Init RCswitch')
 		self.setupOK = True
 		self._sh=smarthome
 		
-		# Check optional Parameters: check Path
-		# Step 1: format string: cut possible '/' at end of rcswitch_dir parameter
+		# format path: cut possible '/' at end of rcswitch_dir parameter
 		if rcswitch_dir[len(rcswitch_dir)-1] == '/':
 			self.rcswitch_dir = rcswitch_dir[0:len(rcswitch_dir)-1]
 		else:
 			self.rcswitch_dir = rcswitch_dir
 
-		# Step 2: check if rc switch is installed at the specified path
-		if not os.path.isfile('{}/send'.format(self.rcswitch_dir)):
-			self.logger.error('RCswitch: send file of RCswitch not found at {}. Check if RCswitch is installed correctly and path is set correctly in logic.conf.'.format(self.rcswitch_dir))
-			self.setupOK = False
-			
 		# Check optional Parameters: check sendDuration
 		try:
 			self.sendDuration = float(rcswitch_sendDuration)
 		except Exception as e:
 			self.sendDuration = float(0.5)
 			self.logger.warning('RCswitch: Argument {} for rcswitch_sendDuration is not a valid number. Using default value instead.'.format(rcswitch_sendDuration))
-			
+		
+		# Handle host
+		if self.is_ip(rcswitch_host) and rcswitch_host != '127.0.0.1':
+			#check connection to remote host and accept fingerprint
+			try:
+				# following line shall raise an error in case connection is not possible. 
+				user = subprocess.check_output(shlex.split('sshpass -p {} ssh -o StrictHostKeyChecking=no {}@{} grep {} /etc/passwd'.format(rcswitch_password, rcswitch_user, rcswitch_host, rcswitch_user)), stderr=DEVNULL).decode('utf8')[0:len(rcswitch_user)]
+				# check  if rc switch is installed at the specified path on remote host
+				fileStat = subprocess.check_output(shlex.split('sshpass -p {} ssh {}@{} stat -c %a {}'.format(rcswitch_password, rcswitch_user, rcswitch_host, self.rcswitch_dir)), stderr=DEVNULL).decode('utf8')
+				self.rcswitch_dir = ('sshpass -p {} ssh {}@{} {}'.format(rcswitch_password, rcswitch_user, rcswitch_host, self.rcswitch_dir))
+				self.logger.info('RCswitch: Using {} as host.'.format(rcswitch_host))
+			except subprocess.CalledProcessError as e:
+				self.setupOK = False
+				# give user hint where the problem is
+				try:
+					if (user == rcswitch_user):
+						self.logger.error('RCswitch: send file of RCswitch not found at {} on {}. Check if RCswitch is installed correctly at specifed path on {}. System returned: {}'.format(self.rcswitch_dir, rcswitch_host, rcswitch_host, e))
+					else:
+						self.logger.error('RCswitch: send file of RCswitch not found at {} on {}. Additional problem with user authentication possible. System returned: {}'.format(self.rcswitch_dir, rcswitch_host, e))
+				except UnboundLocalError as e:
+					self.logger.error('RCswitch: Cannot connect to {}. Check rcswitch_host, rcswitch_user and rcswitch_password are set (correctly). Ensure SSH server is running on {}. System returned: {}'.format(rcswitch_host, rcswitch_host, e))
+		else:
+			# check if rc switch is installed at the specified path
+			if not os.path.isfile('{}/send'.format(self.rcswitch_dir)):
+				self.logger.error('RCswitch: send file of RCswitch not found at {} on localhost. Check path, if RCswitch is installed correctly on target, and correct format of v4 IP adress (in case rcswitch_host is defined).'.format(self.rcswitch_dir))
+				self.setupOK = False
+			else:
+				self.logger.info('RCswitch: setup on localhost OK')
+		
 		# setup semaphore
 		self.lock = threading.Lock()
 		
