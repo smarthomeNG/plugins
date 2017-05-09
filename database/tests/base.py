f6d15eb4137efd0cfea1a00d51389cd244ab837e
Oliver Hinckel <info@ollisnet.de>
2017-05-09 21:21:38 +0200

    - more tests and helper methods for database plugin

--- database/tests/base.py
+++ database/tests/base.py
@@ -1,10 +1,13 @@
 import common
+import datetime
 import unittest
 
 from plugins.database import Database
 from tests.mock.core import MockSmartHome
 
 class TestDatabaseBase(unittest.TestCase):
+
+    TIME_FACTOR = 1000
 
     def plugin(self):
         self.sh = MockSmartHome()
@@ -15,19 +18,69 @@ class TestDatabaseBase(unittest.TestCase):
             plugin.parse_item(item)
         return plugin
 
+    def t(self, s):
+        return s * TestDatabaseBase.TIME_FACTOR;
+
     def create_item(self, plugin, name):
         return plugin.id(self.sh.return_item(name), True)
 
     def create_log(self, plugin, name, tuples):
         id = self.create_item(plugin, name)
         for t in tuples:
-            plugin.insertLog(id, time=t[0], duration=t[1]-t[0], val=t[2], it='num')
+            plugin.insertLog(id, time=self.t(t[0]), duration=self.t(t[1]-t[0]), val=t[2], it='num')
+
+    def dump_log(self, plugin, name):
+        print(":::::::")
+        print(plugin.readLogs(plugin.id(self.sh.return_item(name), False)))
+        print(":::::::")
+
+    def log_slice(self, start, interval, *tuples_list):
+        logs = []
+        for tuples in tuples_list:
+            for t in tuples:
+                logs.append((start, start + interval, t))
+                start = start + interval
+        return logs
+
+    def log_slice_values_delta(self, start, end, delta):
+        values = []
+        value = start
+        while (delta < 0 or value <= end) and (delta > 0 or value >= end):
+            values.append(value)
+            value = value + delta
+        return values
+
+    def log_slice_values_func(self, start, end, func):
+        n = 0
+        value = func(n=n)
+        while value <= end:
+            values.append(value)
+            n = n + 1
+            value = func(n)
+        return values
+
+    def log_dump(self, values):
+        func = [
+           lambda v, nv: "{0:5} - {1: >5} ({2: >3})".format(v, (nv if nv != None else 0), (nv if nv != None else 0)-v),
+           lambda v, nv: v,
+           lambda v, nv: v
+        ]
+        align = [
+           ">26", ">10", ">10"
+        ]
+        for (j, value) in enumerate(values):
+           for (i, column) in enumerate(value):
+               fmt = "{0: " + align[i] + "}"
+               v = column
+               nv = None if j == len(values)-1 else values[j+1][i]
+               print(fmt.format(func[i](v, nv)), end='')
+           print("");
 
     def assertSingle(self, expected, actual):
         self.assertEquals(expected, actual)
 
     def assertSeries(self, expected, actual):
-        # Series result is:
+        # Series result is (in actual):
         # {
         # 'sid': 'main.num|avg|0|now|100',
         # 'params': {
@@ -46,7 +99,10 @@ class TestDatabaseBase(unittest.TestCase):
         # 'update': datetime.datetime(2017, 10, 26, 16, 27, 16, 33702),
         # 'cmd': 'series'
         # }
-        self.assertEquals(expected, actual['series'])
+        result = []
+        for (i, e) in enumerate(expected):
+            result.append((self.t(e[0]), e[1]))
+        self.assertEquals(result, actual['series'])
 
     def assertSeriesCount(self, expected, actual):
         self.assertEquals(expected, len(actual['series']))
