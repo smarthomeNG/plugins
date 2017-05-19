4a5de91c68f244e23b12052d26cf952657fb084e
Markus Garscha <mg@gama.de>
2017-05-19 13:22:44 +0200

    some changes - see readme

--- telegram/__init__.py
+++ telegram/__init__.py
@@ -3,23 +3,25 @@
 #########################################################################
 # Copyright 2017 Markus Garscha                 http://knx-user-forum.de/
 #########################################################################
-#  This file is part of SmartHome.py.    http://mknx.github.io/smarthome/
 #
-#  SmartHome.py is free software: you can redistribute it and/or modify
+#  This file is part of SmartHomeNG.
+#
+#  SmartHomeNG is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
-#  SmartHome.py is distributed in the hope that it will be useful,
+#  SmartHomeNG is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with SmartHome.py. If not, see <http://www.gnu.org/licenses/>.
+#  along with SmartHomeNG. If not, see <http://www.gnu.org/licenses/>.
+#
 #########################################################################
 
-#### Setup Phyton lib for telegram
+#### Setup Phyton lib for telegram manually
 # apt-get install python-pip
 # pip install telepot
 
@@ -27,15 +29,27 @@ import time
 import random
 import datetime
 import logging
+import urllib3
 import telepot
+import telepot.api
 from lib.model.smartplugin import SmartPlugin
 
 PLUGIN_ATTR_TOKEN		= 'token'
 PLUGIN_ATTR_CHAT_IDS	= 'trusted_chat_ids'
+
 ITEM_ATTR_MESSAGE		= 'telegram_message'
+ITEM_ATTR_MATCHREGEX		= 'telegram_value_match_regex'
+
+MESSAGE_TAG_ID          = '[ID]'
+MESSAGE_TAG_NAME        = '[NAME]'
+MESSAGE_TAG_VALUE       = '[VALUE]'
+MESSAGE_TAG_CALLER      = '[CALLER]'
+MESSAGE_TAG_SOURCE      = '[SOURCE]'
+MESSAGE_TAG_DEST        = '[DEST]'
+
 
 class Telegram(SmartPlugin):
-    PLUGIN_VERSION = "1.1.2"
+    PLUGIN_VERSION = "1.1.3"
     ALLOW_MULTIINSTANCE = False
 
     # Storage Array for all items using telegram attributes
@@ -43,9 +57,17 @@ class Telegram(SmartPlugin):
     _chat_ids = []
 
     # called, before items are loaded
-    def __init__(self, smarthome, token='dummy', trusted_chat_ids='none'):
+    def __init__(self, smarthome, token='dummy', trusted_chat_ids='none', name='SH Telegram Gateway', welcome_msg='SmarthomeNG Telegram Plugin is up and running'):
         self._sh = smarthome
         self.logger = logging.getLogger(__name__)
+
+	# Really don't need to hear about connections being brought up again after server has closed it
+        # use logging.yaml or
+        # logging.getLogger("requests.packages.urllib3.connectionpool").setLevel(logging.WARNING)
+
+        telepot.api._pools = {
+            'default': urllib3.PoolManager(num_pools=3, maxsize=10, retries=3, timeout=30),
+        }
 
         self._bot = telepot.Bot(token)
         self.logger.info("Telegram bot is listening: {0}".format(self._bot.getMe()))
@@ -56,7 +78,10 @@ class Telegram(SmartPlugin):
 
         if len(self._chat_ids) < 1:
             self.logger.info("No trusted chat ids configured!")
-        self._name = "GAMA HOME"
+        # self._name = "GAMA HOME"
+        self._name = name
+
+        self._msg_broadcast(welcome_msg) 
 
     # triggered by sh.telegram(msg)
     def __call__(self, msg):
@@ -94,14 +119,53 @@ class Telegram(SmartPlugin):
             self.logger.info("update item: {0}".format(item.id()))
 
         if ITEM_ATTR_MESSAGE in item.conf:
-            msg = item.conf[ITEM_ATTR_MESSAGE]
-            self.logger.info("send Message: {}".format(msg))
-            for cid in self._chat_ids:
-                self._bot.sendMessage(cid, msg)
+            msg_txt_tmpl = item.conf[ITEM_ATTR_MESSAGE]
 
-    def _broadcast(msg):
+            item_id = item.id()
+            item_value = "{0}".format(item())
+
+            # TODO: ITEM_ATTR_MATCHREGEX
+            # p = re.compile('\d+')
+            # m = p.match( 'string goes here' )
+            # if m:
+            #    print('Match found: ', m.group())
+            # else:
+            #    print('No match')
+
+            caller = "None" if caller is None else str(caller)
+            source = "None" if source is None else str(source)
+            dest = "None" if dest is None else str(dest)
+
+            if 'name' in item.conf:
+                item_name = item.conf['name']
+            else:
+                item_name = 'NONAME'
+
+            # TODO: item.__changed_by
+
+            # replace Tags with id,value,caller,source,dest,... 
+            msg_txt = msg_txt_tmpl.replace(MESSAGE_TAG_ID, item_id)
+            msg_txt = msg_txt.replace(MESSAGE_TAG_NAME, item_name)
+            msg_txt = msg_txt.replace(MESSAGE_TAG_VALUE, item_value)
+            msg_txt = msg_txt.replace(MESSAGE_TAG_CALLER, caller)
+            msg_txt = msg_txt.replace(MESSAGE_TAG_SOURCE, source)
+            msg_txt = msg_txt.replace(MESSAGE_TAG_DEST, dest)
+
+            # DEBUG
+            # msg_txt = msg_txt_tmpl
+
+            self.logger.info("send Message: {}".format(msg_txt))
+
+            self._msg_broadcast(msg_txt)
+            # for cid in self._chat_ids:
+            #    self._bot.sendMessage(cid, msg_txt)
+
+    def _msg_broadcast(self, msg):
         for cid in self._chat_ids:
-            self._bot.sendMessage(cid, msg)
+            try:
+                self._bot.sendMessage(cid, msg)
+            except:
+                self.logger.error("could not broadcast to chat id [%d]" % cid)
 
     def message_handler(self, msg):
         self._chat_id = msg['chat']['id']
@@ -129,10 +193,7 @@ class Telegram(SmartPlugin):
 
         # /list: show registered items and value
         elif command == '/list':
-            self.list_items()
-            # self._bot.sendMessage(self._chat_id, "<b>bold</b> and <i>italic</i>", parse_mode='HTML')
-            # self._bot.sendMessage(self._chat_id, "|ABC|DEF|\n|abc|def|", parse_mode='Markdown')
-            # self._bot.sendMessage(self._chat_id, "*bold* _italic_ ~deleted~ `code` ````code\nblock```  [link](http://www.google.com)", parse_mode='Markdown')
+            self.list_items(self._chat_id)
 
         # /subscribe: TODO: subscribe to bot
         elif command == '/subscribe':
@@ -144,7 +205,7 @@ class Telegram(SmartPlugin):
                 self.logger.info("[%d] NOT found in registered IDs: [%s]" % (tmp_chat_id, ''.join(map(str, self._chat_ids))))
                 pos = -1 
             if pos >= 0: # registered chat_id
-                self._bot.sendMessage(tmp_chat_id, "Welcome at %s! Your are signed up already" % self._name)
+                self._bot.sendMessage(tmp_chat_id, "Welcome at %s! Your are signed up already with chat id [%d]" % (self._name,tmp_chat_id))
             else:
                 self._bot.sendMessage(tmp_chat_id, "Welcome at %s. Please register your ID: [%d]" % (self._name, tmp_chat_id))
         elif command == '/test':
@@ -152,7 +213,7 @@ class Telegram(SmartPlugin):
         else:
             self._bot.sendMessage(tmp_chat_id, "unkown command %s" % (command))
 
-    def list_items(self):
+    def list_items(self, chat_id):
         text = ""
         for item in self._items:
             if item.type():
@@ -160,7 +221,10 @@ class Telegram(SmartPlugin):
             else:
                 text += "{0}\n".format(item.id())
 
-        self._bot.sendMessage(self._chat_id, text)
+        # self._bot.sendMessage(self._chat_id, "<b>bold</b> and <i>italic</i>", parse_mode='HTML')
+        # self._bot.sendMessage(self._chat_id, "|ABC|DEF|\n|abc|def|", parse_mode='Markdown')
+        # self._bot.sendMessage(self._chat_id, "*bold* _italic_ ~deleted~ `code` ````code\nblock```  [link](http://www.google.com)", parse_mode='Markdown')
+        self._bot.sendMessage(chat_id, text)
 
 # if __name__ == '__main__':
 #     logging.basicConfig(level=logging.DEBUG)
