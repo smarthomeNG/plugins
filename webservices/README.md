329a89a063ac5e48eeae2c0ed2a940c5b6962a64
Marc René Frieß <rene.friess@gmx.de>
2017-08-19 11:22:58 +0200

    WebServices: added simple service interface

--- webservices/README.md
+++ webservices/README.md
@@ -32,17 +32,64 @@ Currently access to all items is provided via the REST api, no setting has to be
 
 ## Usage
 
+### General: Error and Success Messages
+
+In case of an error (e.g. item is not found), the plugin returns an error formatted as JSON:
+
+{"Error": "No item with item path offifce.light found."}
+
+In case a request is successful, it returns a SUCCESS message as JSON.
+
+### Simple Interface
+
+#### Get
+
+Gets the value of an item.
+
+http://<your_server_ip>:<your_backend_port>/ws/get/<item_path>
+
+E.g. http://192.168.178.100:1234/ws/get/office.light returns "True" when the light is on.
+
+#### Set
+
+Sets a value of an item.
+
+http://<your_server_ip>:<your_backend_port>/ws/set/<item_path>/<value>
+
+E.g. http://192.168.178.100:1234/ws/set/office.light/0 or http://192.168.178.100:1234/ws/set/office.light/False turns off the light.
+
+#### Details
+
+Returns detail data of the item.
+
+http://<your_server_ip>:<your_backend_port>/ws/details/<item_path>
+
+E.g. http://192.168.178.100:1234/ws/details/office.light
+
+[{"name": "office.light", "last_change": "2017-08-18 12:40:33.544449+02:00", "threshold": "False", "crontab": "", 
+"enforce_updates": "False", "eval_trigger": "False", "autotimer": "False", "logics": "[]", 
+"config": "{\"alexa_actions\": \"turnOn turnOff\", \"alexa_name\": \"Lampe B\\u00fcro\", \"knx_dpt\": \"1\", 
+\"knx_init\": \"2/3/50\", \"knx_listen\": \"2/3/50\", \"knx_send\": [\"2/3/10\"], \"nw\": \"yes\", \"sim\": \"track\", 
+\"visu_acl\": \"rw\"}", "previous_change": "2017-08-18 15:12:36.970801+02:00", "eval": "None", "cycle": "", 
+"previous_age": "", "triggers": "[\"bound method KNX.update_item of plugins.knx.KNX\", 
+\"bound method WebSocket.update_item of plugins.visu_websocket.WebSocket\", 
+\"bound method Simulation.update_item of plugins.simulation.Simulation\"]", "path": "office.light", "age": 9123.543011, 
+"type": "bool", "last_update": "2017-08-18 12:40:33.544449+02:00", "changed_by": "Cache", 
+"cache": "/usr/local/smarthome/var/cache/office.light"}]
+
+### REST Compliant Interface
+
 http://<your_server_ip>:<your_backend_port>/rest/item/<item_path>
 http://<your_server_ip>:<your_backend_port>/rest/items/
 
-### HTTP GET (e.g. normal access to the URL)
+#### HTTP GET (e.g. normal access to the URL)
 
 Gets the value of an item.
 
 E.g. http://192.168.178.100:1234/rest/item/office.light returns "True" when the light is on.
 E.g. http://192.168.178.100:1234/rest/items/ return the list of all available items.
 
-### HTTP PUT
+#### HTTP PUT
 
 A HTTP PUT request to the URL sets a value of an item. Only num, bool and str item types are supported.
 For bool items you can use int values 0 and 1, but also "yes", "no", "y", "n", "true", "false", "t", "f", "on", "off".
@@ -50,12 +97,8 @@ In case you send a string (or a string bool representation), take care it is pro
 
 http://<your_server_ip>:<your_backend_port>/rest/item/<item_path>/<value>
 
-E.g. http://192.168.178.100:1234/rest/item/office.light/0 turns of the light.
+E.g. http://192.168.178.100:1234/rest/item/office.light/0 turns off the light.
 
-### General
 
-In case of an error (e.g. item is not found), the plugin returns an error formatted as JSON:
 
-{"Error": "No item with item path offifce.light found."}
-
-In case a PUT request is successful, it returns a SUCCESS message as JSON.
+### URL based interface
--- webservices/__init__.py
+++ webservices/__init__.py
@@ -58,12 +58,19 @@ class WebServices(SmartPlugin):
             }
         }
 
-        # Register the web interface as a cherrypy app
+        # Register the REST interface as a cherrypy app
         self.mod_http.register_app(RESTWebServicesInterface(webif_dir, self),
                                    'rest',
                                    config,
                                    self.get_classname(), self.get_instance_name(),
-                                   description='REST-Plugin für SmartHomeNG')
+                                   description='WebService Plugin für SmartHomeNG (REST)')
+
+        # Register the simple WebService interface as a cherrypy app
+        self.mod_http.register_app(SimpleWebInterface(webif_dir, self),
+                                   'ws',
+                                   config,
+                                   self.get_classname(), self.get_instance_name(),
+                                   description='Webservice-Plugin für SmartHomeNG (simple)')
 
     def run(self):
         """
@@ -79,6 +86,100 @@ class WebServices(SmartPlugin):
         self.logger.debug("Plugin '{}': stop method called".format(self.get_shortname()))
         self.alive = False
 
+class SimpleWebInterface():
+
+    def __init__(self, webif_dir, plugin):
+        self.webif_dir = webif_dir
+        self.logger = logging.getLogger(__name__)
+        self.plugin = plugin
+
+    @cherrypy.expose
+    def get(self, item_path):
+        """
+        get item values
+        """
+        item = self.plugin._sh.return_item(item_path)
+        if item is not None:
+            return json.dumps(item())
+        else:
+            return json.dumps({"Error": "No item with item path %s found."%item_path})
+
+    @cherrypy.expose
+    def set(self, item_path, value):
+        """
+        set item value
+        """
+        item = self.plugin._sh.return_item(item_path)
+        if item is not None:
+            item(value)
+            return json.dumps({"Success": "Item with item path %s set to %s." % (item_path, value)})
+        else:
+            return json.dumps({"Error": "No item with item path %s found." % item_path})
+
+    @cherrypy.expose
+    def details(self, item_path):
+        """
+        get item meta data
+        """
+        item_data = []
+        item = self.plugin._sh.return_item(item_path)
+        if item is not None:
+            cycle = ''
+            crontab = ''
+            for entry in self.plugin._sh.scheduler._scheduler:
+                if entry == item._path:
+                    if self.plugin._sh.scheduler._scheduler[entry]['cycle']:
+                        cycle = self.plugin._sh.scheduler._scheduler[entry]['cycle']
+                    if self.plugin._sh.scheduler._scheduler[entry]['cron']:
+                        crontab = str(self.plugin._sh.scheduler._scheduler[entry]['cron'])
+                    break
+
+            changed_by = item.changed_by()
+            if changed_by[-5:] == ':None':
+                changed_by = changed_by[:-5]
+
+            item_conf_sorted = collections.OrderedDict(sorted(item.conf.items(), key=lambda t: str.lower(t[0])))
+
+            if item.prev_age() < 0:
+                prev_age = ''
+            else:
+                prev_age = self.disp_age(item.prev_age())
+
+            logics = []
+            for trigger in item.get_logic_triggers():
+                logics.append(format(trigger))
+            triggers = []
+            for trigger in item.get_method_triggers():
+                trig = format(trigger)
+                trig = trig[1:len(trig) - 27]
+                triggers.append(format(trig.replace("<", "")))
+
+            data_dict = {'path': item._path,
+                         'name': item._name,
+                         'type': item.type(),
+                         'age': item.age(),
+                         'last_update': str(item.last_update()),
+                         'last_change': str(item.last_change()),
+                         'changed_by': changed_by,
+                         'previous_age': prev_age,
+                         'previous_change': str(item.prev_change()),
+                         'enforce_updates': str(item._enforce_updates),
+                         'cache': str(item._cache),
+                         'eval': str(item._eval),
+                         'eval_trigger': str(item._eval_trigger),
+                         'cycle': str(cycle),
+                         'crontab': str(crontab),
+                         'autotimer': str(item._autotimer),
+                         'threshold': str(item._threshold),
+                         'config': json.dumps(item_conf_sorted),
+                         'logics': json.dumps(logics),
+                         'triggers': json.dumps(triggers)
+            }
+
+            item_data.append(data_dict)
+            return json.dumps(item_data)
+        else:
+            return json.dumps({"Error": "No item with item path %s found." % item_path})
 
 class RESTWebServicesInterface():
     exposed = True
