4cf9fb382ebc4c63792ef115c0ccf1d81e2014cd
psilo909 <rene.friess@gmx.de>
2018-05-02 18:39:00 +0200

    Backend: reference to global smarthomeng.js file

--- backend/webif/static/js/backend.js
+++ backend/webif/static/js/backend.js
@@ -1,12 +1,3 @@
-function resizeCodeMirror(codeMirrorInstance, bottomSpace) {
-    if (!codeMirrorInstance.getOption("fullScreen")) {
-        var browserHeight = $( window ).height();
-        offsetTop = $('.CodeMirror').offset().top;
-        codeMirrorInstance.getScrollerElement().style.maxHeight = ((-1)*(offsetTop) - bottomSpace + browserHeight)+ 'px';
-        codeMirrorInstance.refresh();
-    }
-}
-
 function switchLineWrapping(codeMirrorInstance) {
 	codeMirrorInstance.setOption('lineWrapping', !codeMirrorInstance.getOption('lineWrapping'));
 	if (codeMirrorInstance.getOption('lineWrapping')) {
