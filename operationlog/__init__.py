a538a99cd89a6ff847f6c2c77a19d1ffa2b4a180
msinn <info1@msinn.de>
2018-05-06 13:27:27 +0200

    OperationLog: Adjusted plugin to shtime API

--- operationlog/__init__.py
+++ operationlog/__init__.py
@@ -26,8 +26,10 @@ import lib.log
 import os
 import pickle
 
-from .AutoBlindLoggerOLog import AbLogger
+from lib.shtime import Shtime
 from lib.model.smartplugin import SmartPlugin
+
+from .AutoBlindLoggerOLog import AbLogger
 
 
 
@@ -41,6 +43,7 @@ class OperationLog(AbLogger, SmartPlugin):
                  mapping=['time', 'thread', 'level', 'message'], items=[], maxlen=50):
         log_directory = "var/log/operationlog/"
         self._sh = smarthome
+        self.shtime = Shtime.get_instance()
         self.name = name
         self.logger = logging.getLogger(__name__)
         if log_directory[0] != "/":
@@ -86,13 +89,13 @@ class OperationLog(AbLogger, SmartPlugin):
         if self._cache is True:
             self._cachefile = self._sh._cache_dir + self._path
             try:
-                self.__last_change, self._logcache = _cache_read(self._cachefile, self._sh._tzinfo)
+                self.__last_change, self._logcache = _cache_read(self._cachefile, self.shtime.tzinfo())
                 self.load(self._logcache)
                 self.logger.debug("OperationLog {}: read cache: {}".format(self.name, self._logcache))
             except Exception:
                 try:
                     _cache_write(self.logger, self._cachefile, self._log.export(int(self._maxlen)))
-                    _cache_read(self._cachefile, self._sh._tzinfo)
+                    _cache_read(self._cachefile, self.shtime.tzinfo())
                     self.logger.info("OperationLog {}: generated cache file".format(self.name))
                 except Exception as e:
                     self.logger.warning("OperationLog {}: problem reading cache: {}".format(self._path, e))
@@ -100,7 +103,7 @@ class OperationLog(AbLogger, SmartPlugin):
     def update_logfilename(self):
         if self.__date == datetime.datetime.today() and self.__fname is not None:
             return
-        now = self._sh.now()
+        now = self.shtime.now()
         self.__fname = self._filepattern.format(**{'name': self.name, 'year': now.year, 'month': now.month, 'day': now.day})
         self.__myLogger.update_logfile(self.__fname)
 
@@ -294,7 +297,7 @@ class OperationLog(AbLogger, SmartPlugin):
             log = []
             for name in self._log.mapping:
                 if name == 'time':
-                    log.append(self._sh.now())
+                    log.append(self.shtime.now())
                 elif name == 'thread':
                     log.append(threading.current_thread().name)
                 elif name == 'level':
