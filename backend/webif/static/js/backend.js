f12e1199eec13e9e8e3cf319c161d6ae1f5e9465
psilo909 <rene.friess@gmx.de>
2018-05-14 18:29:48 +0200

    Backend: added code for restart check

--- backend/webif/static/js/backend.js
+++ backend/webif/static/js/backend.js
@@ -6,3 +6,33 @@ function switchLineWrapping(codeMirrorInstance) {
 		$('#linewrapping').removeClass('active');
 	}
 };
+
+function triggerRestart() {
+	/* get Request to start restart of shng */
+	// ...
+	var waitInMS = 2000;
+	setTimeout(function() {
+		checkBackendAvailability();
+	}, waitInMS);
+}
+
+function checkBackendAvailability() {
+	var reload = false;
+
+	while (!reload) {
+        $.ajax({
+        	'url': "/backend/system.html",
+			'async': false,
+        	'type': "GET",
+			'global': false,
+        	'dataType': 'html',
+			'success': function (data) {
+       	         if (data.includes("SmartHomeNG")) {
+                    reload = true;
+                }
+            }
+		});
+    }
+    window.location.href = "/backend/system.html";
+}
+
