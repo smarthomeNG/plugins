886d92e1ccdca07dd8181c97e3b4d46241ca0f69
Bernd Meiners <Bernd.Meiners@mail.de>
2018-07-14 21:13:05 +0200

    DLMS: little bugfix

--- dlms/__init__.py
+++ dlms/__init__.py
@@ -368,5 +368,5 @@ class WebInterface(SmartPluginWebIf):
         """
         tmpl = self.tplenv.get_template('index.html')
         # add values to be passed to the Jinja2 template eg: tmpl.render(p=self.plugin, interface=interface, ...)
-        return tmpl.render(p=self.plugin, i=self.plugin._instance, c=self.plugin._config, r=self.plugin._last_readout, cycle=self._update_cycle)
+        return tmpl.render(p=self.plugin, i=self.plugin._instance, c=self.plugin._config, r=self.plugin._last_readout, cycle=self.plugin._update_cycle)
 
