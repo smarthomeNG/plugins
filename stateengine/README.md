b65f01a4fd2e4179c748c6a84105d7c57627e64a
Onkel Andy <onkelandy@hotmail.com>
2018-10-06 14:21:15 +0200

    Added documentation via web interface from i-am-offlines Wiki

--- stateengine/README.md
+++ stateengine/README.md
@@ -1,7 +1,7 @@
-#StateEngine
+# StateEngine
 Created by i-am-offline
 
-##Description
+## Description
 Finite state machine plugin for smarthomeNG, previously known as AutoBlind
 
 ## Support
@@ -18,16 +18,17 @@ See the [manual at GitHub](https://github.com/i-am-offline/smarthome.plugin.auto
 ### items.yaml
 See the [manual at GitHub](https://github.com/i-am-offline/smarthome.plugin.autoblind/wiki)
 
-##Changelog
+## Changelog
 ### v1.4.1
-Added to official develop repository (onkelandy)
-Renamed to StateEngine (onkelandy)
-Fix compatibility of logic trigger for SmarthomeNG 1.4+ (onkelandy)
-Changed state condition evaluation to OrderedDict to keep original order (onkelandy)
-Added additional option for manual item called "manual_on" to figure out if item WAS trigger by specific KNX GA (onkelandy)
+* Added to official develop repository (onkelandy)
+* Renamed to StateEngine (onkelandy)
+* Fixed compatibility of logic trigger for SmarthomeNG 1.4+ (onkelandy)
+* Changed state condition evaluation to OrderedDict to keep original order (onkelandy)
+* Added additional option for manual item called "manual_on" to figure out if item WAS trigger by specific KNX GA (onkelandy)
+* Added Webinterface with documentation (german)
 
 ### v1.4.0
-Make compatible with SmarthomeNG 1.4+ (i-am-offline)
+* Make compatible with SmarthomeNG 1.4+ (i-am-offline)
 
 ### before v1.4.0
-Constant improvements to infinite state machine (complete plugin development by i-am-offline)
+* Constant improvements to infinite state machine (complete plugin development by i-am-offline)
--- stateengine/__init__.py
+++ stateengine/__init__.py
@@ -27,7 +27,7 @@ from . import StateEngineCliCommands
 from . import StateEngineFunctions
 import logging
 import os
-from lib.model.smartplugin import SmartPlugin
+from lib.model.smartplugin import *
 from bin.smarthome import VERSION
 
 
@@ -43,37 +43,41 @@ class StateEngine(SmartPlugin):
         self.__items = {}
         self.alive = False
         self.__cli = None
+        self.init_webinterface()
+        try:
+            log_level = self.get_parameter_value("log_level")
+            log_directory = self.get_parameter_value("log_directory")
+            self.logger.info("Init StateEngine (log_level={0}, log_directory={1})".format(log_level, log_directory))
 
-        log_level = self.get_parameter_value("log_level")
-        log_directory = self.get_parameter_value("log_directory")
-        self.logger.info("Init StateEngine (log_level={0}, log_directory={1})".format(log_level, log_directory))
+            StateEngineDefaults.startup_delay = self.get_parameter_value("startup_delay_default")
+            StateEngineDefaults.suspend_time = self.get_parameter_value("suspend_time_default")
+            StateEngineDefaults.write_to_log(self.logger)
 
-        StateEngineDefaults.startup_delay = self.get_parameter_value("startup_delay_default")
-        StateEngineDefaults.suspend_time = self.get_parameter_value("suspend_time_default")
-        StateEngineDefaults.write_to_log(self.logger)
+            StateEngineCurrent.init(self.get_sh())
 
-        StateEngineCurrent.init(self.get_sh())
+            if log_level > 0:
+                if log_directory[0] != "/":
+                    base = self.get_sh().get_basedir()
+                    if base[-1] != "/":
+                        base += "/"
+                    log_directory = base + log_directory
+                if not os.path.exists(log_directory):
+                    os.makedirs(log_directory)
+                SeLogger.set_loglevel(log_level)
+                SeLogger.set_logdirectory(log_directory)
+                text = "StateEngine extended logging is active. Logging to '{0}' with loglevel {1}."
+                self.logger.info(text.format(log_directory, log_level))
+            log_maxage = self.get_parameter_value("log_maxage")
+            if log_level > 0 and log_maxage > 0:
+                self.logger.info("StateEngine extended log files will be deleted after {0} days.".format(log_maxage))
+                SeLogger.set_logmaxage(log_maxage)
+                cron = ['init', '30 0 * *']
+                self.scheduler_add('StateEngine: Remove old logfiles', SeLogger.remove_old_logfiles, cron=cron, offset=0)
 
-        if log_level > 0:
-            if log_directory[0] != "/":
-                base = self.get_sh().get_basedir()
-                if base[-1] != "/":
-                    base += "/"
-                log_directory = base + log_directory
-            if not os.path.exists(log_directory):
-                os.makedirs(log_directory)
-            SeLogger.set_loglevel(log_level)
-            SeLogger.set_logdirectory(log_directory)
-            text = "StateEngine extended logging is active. Logging to '{0}' with loglevel {1}."
-            self.logger.info(text.format(log_directory, log_level))
-        log_maxage = self.get_parameter_value("log_maxage")
-        if log_level > 0 and log_maxage > 0:
-            self.logger.info("StateEngine extended log files will be deleted after {0} days.".format(log_maxage))
-            SeLogger.set_logmaxage(log_maxage)
-            cron = ['init', '30 0 * *']
-            self.scheduler_add('StateEngine: Remove old logfiles', SeLogger.remove_old_logfiles, cron=cron, offset=0)
-
-        self.get_sh().stateengine_plugin_functions = StateEngineFunctions.SeFunctions(self.get_sh(), self.logger)
+            self.get_sh().stateengine_plugin_functions = StateEngineFunctions.SeFunctions(self.get_sh(), self.logger)
+        except Exception:
+            self._init_complete = False
+            return
 
     # Parse an item
     # noinspection PyMethodMayBeStatic
@@ -136,3 +140,85 @@ class StateEngine(SmartPlugin):
                             entry_source == original_source or entry_source == "*"):
                 return False
         return True
+
+    def init_webinterface(self):
+        """"
+        Initialize the web interface for this plugin
+
+        This method is only needed if the plugin is implementing a web interface
+        """
+        try:
+            self.mod_http = Modules.get_instance().get_module('http')   # try/except to handle running in a core version that does not support modules
+        except:
+             self.mod_http = None
+        if self.mod_http == None:
+            self.logger.error("Plugin '{}': Not initializing the web interface".format(self.get_shortname()))
+            return False
+
+        import sys
+        if not "SmartPluginWebIf" in list(sys.modules['lib.model.smartplugin'].__dict__):
+            self.logger.warning("Plugin '{}': Web interface needs SmartHomeNG v1.5 and up. Not initializing the web interface".format(self.get_shortname()))
+            return False
+
+        # set application configuration for cherrypy
+        webif_dir = self.path_join(self.get_plugin_dir(), 'webif')
+        config = {
+            '/': {
+                'tools.staticdir.root': webif_dir,
+            },
+            '/static': {
+                'tools.staticdir.on': True,
+                'tools.staticdir.dir': 'static'
+            }
+        }
+
+        # Register the web interface as a cherrypy app
+        self.mod_http.register_webif(WebInterface(webif_dir, self),
+                                     self.get_shortname(),
+                                     config,
+                                     self.get_classname(), self.get_instance_name(),
+                                     description='')
+
+        return True
+
+
+# ------------------------------------------
+#    Webinterface of the plugin
+# ------------------------------------------
+
+import cherrypy
+from jinja2 import Environment, FileSystemLoader
+
+class WebInterface(SmartPluginWebIf):
+
+
+    def __init__(self, webif_dir, plugin):
+        """
+        Initialization of instance of class WebInterface
+
+        :param webif_dir: directory where the webinterface of the plugin resides
+        :param plugin: instance of the plugin
+        :type webif_dir: str
+        :type plugin: object
+        """
+        self.logger = logging.getLogger(__name__)
+        self.webif_dir = webif_dir
+        self.plugin = plugin
+        self.tplenv = self.init_template_environment()
+
+
+    @cherrypy.expose
+    def index(self, action=None, item_id=None, item_path=None, reload=None, page='index'):
+        """
+        Build index.html for cherrypy
+
+        Render the template and return the html file to be delivered to the browser
+
+        :return: contents of the template after beeing rendered
+        """
+        item = self.plugin.get_sh().return_item(item_path)
+
+        tmpl = self.tplenv.get_template('{}.html'.format(page))
+        # add values to be passed to the Jinja2 template eg: tmpl.render(p=self.plugin, interface=interface, ...)
+        return tmpl.render(p=self.plugin,
+                           language=self.plugin._sh.get_defaultlanguage(), now=self.plugin.shtime.now())
