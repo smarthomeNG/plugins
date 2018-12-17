36a7732ae463d8cb272ec3c1b4f08494a9d92904
psilo909 <rene.friess@gmx.de>
2018-12-17 15:52:08 +0100

    AVM: set minversion to 1.5b, set normal version to 1.5.6

--- avm/__init__.py
+++ avm/__init__.py
@@ -505,7 +505,7 @@ class AVM(SmartPlugin):
     Main class of the Plugin. Does all plugin specific stuff and provides the update functions for the different TR-064 services on the FritzDevice
     """
 
-    PLUGIN_VERSION = "1.5.5"
+    PLUGIN_VERSION = "1.5.6"
 
     _header = {'SOAPACTION': '', 'CONTENT-TYPE': 'text/xml; charset="utf-8"'}
     _envelope = """
