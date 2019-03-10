7e9addeee6455cfe1dd482c4b68d0a4aae00479d
Thomas Creutz <thomas.creutz@gmx.de>
2019-03-10 15:10:54 +0100

    fix setting to zero too

--- rtr/__init__.py
+++ rtr/__init__.py
@@ -330,7 +330,7 @@ class RTR(SmartPlugin):
                     if item():
                        if self._items.return_item(self._controller[c]['actuatorItem'])() > 0:
                            logger.info("rtr: controller {0} stopped, because of item {1}".format(c, item.id()))
-                       self._controller[c]['actuatorItem'](0)
+                       self._items.return_item(self._controller[c]['actuatorItem'])(0)
 
 
     def update_HVACMode(self, item, caller=None, source=None, dest=None):
