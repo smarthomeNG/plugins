95e2c37af86a322aea01fee7df9060b4f4ece019
onkelandy <onkelandy@hotmail.com>
2019-05-12 15:45:15 +0200

    several fixes
    - turn_off and muteoff now work
    - handling multiple responses at once
    - resend if response not correct
    - init on reconnect

--- kodi/__init__.py
+++ kodi/__init__.py
@@ -25,6 +25,7 @@
 import logging
 import threading
 import json
+import re
 
 from lib.model.smartplugin import SmartPlugin
 from lib.network import Tcp_client
@@ -36,10 +37,10 @@ class Kodi(SmartPlugin):
     Main class of the Plugin. Does all plugin specific stuff and provides
     the update functions for the items
     '''
-    
-    PLUGIN_VERSION = '1.3c.0'
+
+    PLUGIN_VERSION = '1.4.0'
     ALLOW_MULTIINSTANCE = True
-    
+
     # list of all possible input actions for Kodi
     _possible_input_actions = [
         'left', 'right', 'up', 'down', 'pageup', 'pagedown', 'select', 'highlight',
@@ -79,12 +80,12 @@ class Kodi(SmartPlugin):
         'zoomgesture', 'rotategesture', 'swipeleft', 'swiperight', 'swipeup', 'swipedown', 'error', 'noop']
 
     _get_items = ['volume', 'mute', 'title', 'media', 'state', 'favorites']
-    
+
     _set_items = {'volume': dict(method='Application.SetVolume', params=dict(volume='ITEM_VALUE')),
                   'mute'  : dict(method='Application.SetMute', params=dict(mute='ITEM_VALUE')),
                   'input' : dict(method='Input.ExecuteAction', params=dict(action='ITEM_VALUE')),
                   'on_off': dict(method='System.Shutdown', params=None)}
-    
+
     def __init__(self, sh, *args, **kwargs):
         '''
         Initalizes the plugin.
@@ -94,13 +95,17 @@ class Kodi(SmartPlugin):
         self.logger.info('Init Kodi Plugin')
         self.host = self.get_parameter_value('host')
         self.port = self.get_parameter_value('port')
+        self.autoreconnect = self.get_parameter_value('autoreconnect')
+        self.connect_retries = self.get_parameter_value('connect_retries')
+        self.connect_cycle = self.get_parameter_value('connect_cycle')
+        self.send_retries = self.get_parameter_value('send_retries')
         self.kodi_tcp_connection = Tcp_client(host=self.host,
                                               port=self.port,
                                               name='KodiTCPConnection',
-                                              autoreconnect=False,
-                                              connect_retries=5,
-                                              connect_cycle=5)
-        self.kodi_tcp_connection.set_callbacks(connected=None,
+                                              autoreconnect=self.autoreconnect,
+                                              connect_retries=self.connect_retries,
+                                              connect_cycle=self.connect_cycle)
+        self.kodi_tcp_connection.set_callbacks(connected=self.connect_to_kodi,
                                                data_received=self.received_data,
                                                disconnected=self.on_disconnect)
         self.kodi_server_alive = False
@@ -108,6 +113,8 @@ class Kodi(SmartPlugin):
 #         self.balance(b'{', b'}')
         self.message_id = 1
         self.response_id = None
+        self.sendingcommand = None
+        self.sendingretries = 0
         self.cmd_lock = threading.Lock()
         self.reply_lock = threading.Condition()
         self.reply = None
@@ -116,9 +123,9 @@ class Kodi(SmartPlugin):
     def run(self):
         '''
         Run method for the plugin
-        '''        
+        '''
         self.logger.debug('Plugin \'{}\': run method called'.format(self.get_shortname()))
-        self.connect_to_kodi()
+        self.connect_to_kodi('run')
         self.alive = True
 
     def stop(self):
@@ -129,18 +136,20 @@ class Kodi(SmartPlugin):
         self.kodi_tcp_connection.close()
         self.kodi_server_alive = False
         self.alive = False
-    
-    def on_connect(self):
+
+    def on_connect(self, by):
         '''
-        This method is called on a succesful connect to Kodi        
+        This method is called on a succesful connect to Kodi
         On a connect first check if the JSON-RPC API is available.
         If this is the case initialize all items with values extracted from Kodi
         '''
         # check if API is available
+        self.logger.debug("Kodi running onconnect started by {}. Connection: {}.".format(by, self.kodi_server_alive))
         result = self.send_kodi_rpc(method='JSONRPC.Ping')
+        self.logger.debug("Kodi self reply: {}.".format(self.reply))
         if result is None or result['result'] != 'pong':
             self.logger.error('Kodi JSON-RPC API not available on {}:{}'.format(self.host, self.port))
-            self.stop()
+            #self.stop()
         else:
             # API available -> init items
             #
@@ -154,39 +163,41 @@ class Kodi(SmartPlugin):
             # get the list of favorites
             result = self.send_kodi_rpc(method='Favourites.GetFavourites',
                                         params=dict(properties=['window', 'path', 'thumbnail', 'windowparameter']))['result']
-            item_dict = dict()                                    
+            item_dict = dict()
             if result['favourites'] is not None:
                 item_dict = {elem['title']: elem for elem in result['favourites']}
             for elem in self.registered_items['favorites']:
-                elem(item_dict, caller='Kodi')        
+                elem(item_dict, caller='Kodi')
             # parse active player (if present)
             self._get_player_info()
-    
-    def on_disconnect(self):
+
+    def on_disconnect(self, obj):
         ''' function called when TCP connection to Kodi is disconnected '''
-        self.logger.debug('Received disconnect from Kodi server')
+        self.logger.debug('Received disconnect from Kodi')
         self.kodi_server_alive = False
         for elem in self.registered_items['on_off']:
             elem(self.kodi_server_alive, caller='Kodi')
-    
-    def connect_to_kodi(self):
+
+    def connect_to_kodi(self, by):
         '''
         try to establish a new connection to Kodi
-        
+
         While this method is called during the start-up phase of the plugin,
         it can also be used to establish a connection to the Kodi server if the
         plugin was initialized before the server went up.
         '''
-        self.kodi_tcp_connection.connect()
-        # we allow for 2 seconds to connect
-        time.sleep(2)
+        self.logger.debug("Kodi connection initialized by {}".format(by))
+        if not self.kodi_tcp_connection.connected():
+            self.kodi_tcp_connection.connect()
+            # we allow for 2 seconds to connect
+            time.sleep(2)
         if not self.kodi_tcp_connection.connected():
             # no connection could be established, Kodi may be offline
             self.logger.info('Could not establish a connection to Kodi Server')
             self.kodi_server_alive = False
         else:
             self.kodi_server_alive = True
-            self.on_connect()            
+            self.on_connect(by)
         for elem in self.registered_items['on_off']:
             elem(self.kodi_server_alive, caller='Kodi')
 
@@ -202,7 +213,7 @@ class Kodi(SmartPlugin):
             self.logger.debug('Plugin \'%s\', instance \'%s\': registering item: %s',
                               self.get_shortname(),
                               self.get_instance_name(),
-                              item)            
+                              item)
             if kodi_item in self.registered_items:
                 self.registered_items[kodi_item].append(item)
             else:
@@ -226,22 +237,22 @@ class Kodi(SmartPlugin):
         :param dest: if given it represents the dest
         '''
         item_value = item()
-        if item_value:
+        if item_value is not None:
             if caller != 'Kodi' and self.has_iattr(item.conf, 'kodi_item'):
                 # update item was triggered from something else then this plugin -> send to Kodi
-                kodi_item = self.get_iattr_value(item.conf, 'kodi_item')                
-                
+                kodi_item = self.get_iattr_value(item.conf, 'kodi_item')
+
                 if kodi_item == 'on_off' and item():
-                    # handle the on_off item as special case:                    
+                    # handle the on_off item as special case:
                     # if item is on, try to establish a connection to Kodi
-                    self.connect_to_kodi()
+                    self.connect_to_kodi('update')
                     # if item is off send shutdown command to Kodi. This is
                     # handled in the standard block below though
                 elif kodi_item in Kodi._set_items:
                     if kodi_item == 'input' and item() not in self._possible_input_actions:
                         self.logger.error('The action \'%s\' for the kodi_item \'input\' is not allowed, skipping!', item_value)
                     else:
-                        self.logger.debug('Plugin \'%s\': update_item ws called with item \'%s\' from caller \'%s\', source \'%s\' and dest \'%s\'',
+                        self.logger.debug('Plugin \'%s\': update_item was called with item \'%s\' from caller \'%s\', source \'%s\' and dest \'%s\'',
                                           self.get_shortname(), item, caller, source, dest)
                         method = self._set_items[kodi_item]['method']
                         params = self._set_items[kodi_item]['params']
@@ -255,11 +266,11 @@ class Kodi(SmartPlugin):
                         self.send_kodi_rpc(method, params, wait=False)
                 else:
                     self.logger.info('kodi_item \'%s\' not in send_keys, skipping!', kodi_item)
-    
+
     def notify(self, title, message, image=None, display_time=10000):
         '''
         Send a notification to Kodi to be displayed on the screen
-        
+
         :param title: the title of the message
         :param message: the message itself
         :param image: an optional image to be displayed alongside the message
@@ -273,8 +284,8 @@ class Kodi(SmartPlugin):
     def send_kodi_rpc(self, method, params=None, message_id=None, wait=True):
         '''
         Send a JSON RPC to Kodi.
-        
-        The  JSON string is extracted from the supplied method and the given parameters.        
+
+        The  JSON string is extracted from the supplied method and the given parameters.
         :param method: the Kodi method to be triggered
         :param params: parameters dictionary
         :param message_id: the message ID to be used. If none, use the internal counter
@@ -298,9 +309,11 @@ class Kodi(SmartPlugin):
                 data = {'jsonrpc': '2.0', 'id': message_id, 'method': method}
             self.reply_lock.acquire()
             self.logger.debug('Kodi sending: {0}'.format(json.dumps(data, separators=(',', ':'))))
+            self.sendingcommand = data
             self.kodi_tcp_connection.send((json.dumps(data, separators=(',', ':')) + '\r\n').encode())
             if wait:
-                self.reply_lock.wait(2)
+                self.logger.debug("Waiting for reply_lock..")
+                self.reply_lock.wait(1)
             self.reply_lock.release()
             reply = self.reply
             self.reply = None
@@ -314,39 +327,52 @@ class Kodi(SmartPlugin):
         This method is called whenever data is received from the connection to
         Kodi.
         '''
-        event = json.loads(data)
-        self.logger.debug('Kodi receiving: {0}'.format(event))
-        if 'id' in event:
-            if event['id'] == self.response_id:
-                self.response_id = None
-                self.reply = event
-            self.reply_lock.acquire()
-            self.reply_lock.notify()
-            self.reply_lock.release()
-            return
-        if 'method' in event:
-            if event['method'] == 'Player.OnPause':
-                for elem in self.registered_items['state']:
-                    elem('Pause', caller='Kodi')
-            elif event['method'] == 'Player.OnStop':
-                for elem in self.registered_items['state']:
-                    elem('Stopped Player', caller='Kodi')
-                for elem in self.registered_items['media']:
-                    elem('', caller='Kodi')
-                for elem in self.registered_items['title']:
-                    elem('', caller='Kodi')
-            elif event['method'] == 'GUI.OnScreensaverActivated':
-                for elem in self.registered_items['state']:
-                    elem('Screensaver', caller='Kodi')
-            if event['method'] in ['Player.OnPlay']:
-                # use a different thread for event handling
-                self.get_sh().trigger('kodi-player-start', self._get_player_info, 'Kodi')
-            elif event['method'] in ['Application.OnVolumeChanged']:
-                for elem in self.registered_items['mute']:
-                    elem(event['params']['data']['muted'], caller='Kodi')
-                for elem in self.registered_items['volume']:
-                    elem(event['params']['data']['volume'], caller='Kodi')
-    
+        self.logger.debug('Kodi receiving: {0}'.format(data))
+        text_data = '[' + re.sub(r'\}\{', '},{', data) + ']'
+        events = json.loads(text_data)
+        for event in events:
+            if len(events) > 1:
+                self.logger.debug('Kodi checking from multianswer: {0}'.format(event))
+            if 'id' in event:
+                self.reply_lock.acquire()
+                if event['id'] == self.response_id or event['result'] == 'pong':
+                    self.response_id = None
+                    self.sendingcommand = None
+                    self.reply = event
+                    self.logger.debug("Kodi command {} sent successfully".format(event['id']))
+                    self.reply_lock.notify()
+                    self.reply_lock.release()
+                else:
+                    self.sendingretries += 1
+                    self.reply_lock.release()
+                    if self.sendingretries <= self.send_retries:
+                        self.logger.debug("Kodi resend {}".format(self.sendingcommand))
+                        self.send_kodi_rpc(self.sendingcommand.get('method'), params=self.sendingcommand.get('params'), message_id=self.sendingcommand.get('id'))
+                    else:
+                        self.logger.warning("Kodi giving up command {}".format(self.sendingcommand))
+            elif 'method' in event:
+                if event['method'] == 'Player.OnPause':
+                    for elem in self.registered_items['state']:
+                        elem('Pause', caller='Kodi')
+                elif event['method'] == 'Player.OnStop':
+                    for elem in self.registered_items['state']:
+                        elem('Stopped Player', caller='Kodi')
+                    for elem in self.registered_items['media']:
+                        elem('', caller='Kodi')
+                    for elem in self.registered_items['title']:
+                        elem('', caller='Kodi')
+                elif event['method'] == 'GUI.OnScreensaverActivated':
+                    for elem in self.registered_items['state']:
+                        elem('Screensaver', caller='Kodi')
+                if event['method'] in ['Player.OnPlay']:
+                    # use a different thread for event handling
+                    self.get_sh().trigger('kodi-player-start', self._get_player_info, 'Kodi')
+                elif event['method'] in ['Application.OnVolumeChanged']:
+                    for elem in self.registered_items['mute']:
+                        elem(event['params']['data']['muted'], caller='Kodi')
+                    for elem in self.registered_items['volume']:
+                        elem(event['params']['data']['volume'], caller='Kodi')
+
     def _send_player_command(self, kodi_item):
         '''
         This method should only be called from the update item method in
@@ -398,7 +424,7 @@ class Kodi(SmartPlugin):
                 elem(typ.capitalize(), caller='Kodi')
         elif typ == 'audio':
             for elem in self.registered_items['media']:
-                elem('Audio', caller='Kodi')                
+                elem('Audio', caller='Kodi')
             result = self.send_kodi_rpc(method='Player.GetItem',
                                         params=dict(properties=['title', 'artist'], playerid=playerid),
                                         message_id='AudioGetItem')['result']
