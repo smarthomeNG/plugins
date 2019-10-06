1ac5fd2cd18c7a62500b70e28c4e325509525d72
Thilo Schneider <freget@googlemail.com>
2019-10-06 17:36:03 +0200

    Added helios_tcp plugin

---
+++ helios_tcp/__init__.py
@@ -0,0 +1,230 @@
+
+import logging
+
+from pymodbus3.client.sync import ModbusTcpClient
+
+from lib.model.smartplugin import SmartPlugin
+
+
+VARLIST = {
+    "outside_temp"              : {"var": "v00104", "length": 8, "type": float, "read": True, "write": False},
+    "exhaust_temp"              : {"var": "v00107", "length": 8, "type": float, "read": True, "write": False},
+    "inside_temp"               : {"var": "v00106", "length": 8, "type": float, "read": True, "write": False},
+    "incoming_temp"             : {"var": "v00105", "length": 8, "type": float, "read": True, "write": False},
+    "pre_heating_temp"          : {"var": "v00108", "length": 8, "type": float, "read": True, "write": False},
+    "post_heating_temp"         : {"var": "v00146", "length": 8, "type": float, "read": True, "write": False},
+    "post_heating_reflux_temp"  : {"var": "v00110", "length": 8, "type": float, "read": True, "write": False},
+    "error_count"               : {"var": "v01300", "length": 5, "type": int,   "read": True, "write": False},
+    "warning_count"             : {"var": "v01301", "length": 5, "type": int,   "read": True, "write": False},
+    "info_count"                : {"var": "v01302", "length": 5, "type": int,   "read": True, "write": False},
+    "fan_in_rpm"                : {"var": "v00348", "length": 6, "type": int,   "read": True, "write": False},
+    "fan_out_rpm"               : {"var": "v00349", "length": 6, "type": int,   "read": True, "write": False},
+    "internal_humidity"         : {"var": "v02136", "length": 6, "type": int,   "read": True, "write": False},
+    "sensor1_humidity"          : {"var": "v00111", "length": 6, "type": int,   "read": True, "write": False},
+    "sensor2_humidity"          : {"var": "v00112", "length": 6, "type": int,   "read": True, "write": False},
+    "sensor3_humidity"          : {"var": "v00113", "length": 6, "type": int,   "read": True, "write": False},
+    "sensor4_humidity"          : {"var": "v00114", "length": 6, "type": int,   "read": True, "write": False},
+    "sensor5_humidity"          : {"var": "v00115", "length": 6, "type": int,   "read": True, "write": False},
+    "sensor6_humidity"          : {"var": "v00116", "length": 6, "type": int,   "read": True, "write": False},
+    "sensor7_humidity"          : {"var": "v00117", "length": 6, "type": int,   "read": True, "write": False},
+    "sensor8_humidity"          : {"var": "v00118", "length": 6, "type": int,   "read": True, "write": False},
+    "sensor1_temperature"       : {"var": "v00119", "length": 8, "type": float, "read": True, "write": False},
+    "sensor2_temperature"       : {"var": "v00120", "length": 8, "type": float, "read": True, "write": False},
+    "sensor3_temperature"       : {"var": "v00121", "length": 8, "type": float, "read": True, "write": False},
+    "sensor4_temperature"       : {"var": "v00122", "length": 8, "type": float, "read": True, "write": False},
+    "sensor5_temperature"       : {"var": "v00123", "length": 8, "type": float, "read": True, "write": False},
+    "sensor6_temperature"       : {"var": "v00124", "length": 8, "type": float, "read": True, "write": False},
+    "sensor7_temperature"       : {"var": "v00125", "length": 8, "type": float, "read": True, "write": False},
+    "sensor8_temperature"       : {"var": "v00126", "length": 8, "type": float, "read": True, "write": False},
+    "sensor1_co2"               : {"var": "v00128", "length": 6, "type": float, "read": True, "write": False},
+    "sensor2_co2"               : {"var": "v00129", "length": 6, "type": float, "read": True, "write": False},
+    "sensor3_co2"               : {"var": "v00130", "length": 6, "type": float, "read": True, "write": False},
+    "sensor4_co2"               : {"var": "v00131", "length": 6, "type": float, "read": True, "write": False},
+    "sensor5_co2"               : {"var": "v00132", "length": 6, "type": float, "read": True, "write": False},
+    "sensor6_co2"               : {"var": "v00133", "length": 6, "type": float, "read": True, "write": False},
+    "sensor7_co2"               : {"var": "v00134", "length": 6, "type": float, "read": True, "write": False},
+    "sensor8_co2"               : {"var": "v00135", "length": 6, "type": float, "read": True, "write": False},
+    "sensor1_voc"               : {"var": "v00136", "length": 6, "type": float, "read": True, "write": False},
+    "sensor2_voc"               : {"var": "v00137", "length": 6, "type": float, "read": True, "write": False},
+    "sensor3_voc"               : {"var": "v00138", "length": 6, "type": float, "read": True, "write": False},
+    "sensor4_voc"               : {"var": "v00139", "length": 6, "type": float, "read": True, "write": False},
+    "sensor5_voc"               : {"var": "v00140", "length": 6, "type": float, "read": True, "write": False},
+    "sensor6_voc"               : {"var": "v00141", "length": 6, "type": float, "read": True, "write": False},
+    "sensor7_voc"               : {"var": "v00142", "length": 6, "type": float, "read": True, "write": False},
+    "sensor8_voc"               : {"var": "v00143", "length": 6, "type": float, "read": True, "write": False},
+    "filter_remaining"          : {"var": "v01033", "length": 9, "type": int,   "read": True, "write": False},
+    "boost_remaining"           : {"var": "v00093", "length": 6, "type": int,   "read": True, "write": False},
+    "sleep_remaining"           : {"var": "v00098", "length": 6, "type": int,   "read": True, "write": False},
+    "fan_level_percent"         : {"var": "v00103", "length": 6, "type": int,   "read": True, "write": False},
+    "bypass_open"               : {"var": "v02119", "length": 5, "type": bool,  "read": True, "write": False},
+    "humidity_control_status"   : {"var": "v00033", "length": 5, "type": int,   "read": True, "write": True, "min": 0, "max": 2},
+    "humidity_control_target"   : {"var": "v00034", "length": 5, "type": int,   "read": True, "write": True, "min": 20, "max": 80},
+    "co2_control_status"        : {"var": "v00037", "length": 5, "type": int,   "read": True, "write": True, "min": 0, "max": 2},
+    "co2_control_target"        : {"var": "v00038", "length": 6, "type": int,   "read": True, "write": True, "min": 300, "max": 2000},
+    "voc_control_status"        : {"var": "v00037", "length": 5, "type": int,   "read": True, "write": True, "min": 0, "max": 2},
+    "voc_control_target"        : {"var": "v00038", "length": 6, "type": int,   "read": True, "write": True, "min": 300, "max": 2000},
+    "comfort_temperature"       : {"var": "v00037", "length": 6, "type": float, "read": True, "write": True, "min": 10, "max": 25},
+    "fan_in_voltage_level1"     : {"var": "v00013", "length": 6, "type": float, "read": True, "write": True, "min": 1.6, "max": 10},
+    "fan_out_voltage_level1"    : {"var": "v00012", "length": 6, "type": float, "read": True, "write": True, "min": 1.6, "max": 10},
+    "fan_in_voltage_level2"     : {"var": "v00015", "length": 6, "type": float, "read": True, "write": True, "min": 1.6, "max": 10},
+    "fan_out_voltage_level2"    : {"var": "v00014", "length": 6, "type": float, "read": True, "write": True, "min": 1.6, "max": 10},
+    "fan_in_voltage_level3"     : {"var": "v00017", "length": 6, "type": float, "read": True, "write": True, "min": 1.6, "max": 10},
+    "fan_out_voltage_level3"    : {"var": "v00016", "length": 6, "type": float, "read": True, "write": True, "min": 1.6, "max": 10},
+    "fan_in_voltage_level4"     : {"var": "v00019", "length": 6, "type": float, "read": True, "write": True, "min": 1.6, "max": 10},
+    "fan_out_voltage_level4"    : {"var": "v00018", "length": 6, "type": float, "read": True, "write": True, "min": 1.6, "max": 10},
+    "manual_mode"               : {"var": "v00101", "length": 5, "type": bool,  "read": True, "write": True, "min": 0, "max": 1},
+    "filter_change"             : {"var": "v01031", "length": 5, "type": bool,  "read": True, "write": True, "min": 0, "max": 1},
+    "filter_changeinterval"     : {"var": "v01032", "length": 5, "type": int,   "read": True, "write": True, "min": 0, "max": 12},
+    "bypass_roomtemperature"    : {"var": "v01035", "length": 5, "type": int,   "read": True, "write": True, "min": 10, "max": 40},
+    "bypass_minoutsidetemp"     : {"var": "v01036", "length": 5, "type": int,   "read": True, "write": True, "min": 5, "max": 20},
+    "fan_level"                 : {"var": "v00102", "length": 5, "type": int,   "read": True, "write": True, "min": 0, "max": 4},
+    "fan_in_level"              : {"var": "v01050", "length": 5, "type": int,   "read": True, "write": True, "min": 0, "max": 4},
+    "fan_out_level"             : {"var": "v01051", "length": 5, "type": int,   "read": True, "write": True, "min": 0, "max": 4},
+    "boost_duration"            : {"var": "v00091", "length": 6, "type": int,   "read": True, "write": True, "min": 5, "max": 180},
+    "boost_level"               : {"var": "v00092", "length": 5, "type": int,   "read": True, "write": True, "min": 0, "max": 4},
+    "boost_on"                  : {"var": "v00094", "length": 5, "type": bool,  "read": True, "write": True, "min": 0, "max": 1},
+    "sleep_duration"            : {"var": "v00096", "length": 6, "type": int,   "read": True, "write": True, "min": 5, "max": 180},
+    "sleep_level"               : {"var": "v00097", "length": 5, "type": int,   "read": True, "write": True, "min": 0, "max": 4},
+    "sleep_on"                  : {"var": "v00099", "length": 5, "type": bool,  "read": True, "write": True, "min": 0, "max": 1},
+    "preheating_status"         : {"var": "v00024", "length": 5, "type": bool,  "read": True, "write": True, "min": 0, "max": 1}
+}
+
+
+class HeliosTCP(SmartPlugin):
+    MODBUS_SLAVE = 180
+    PORT = 502
+    START_REGISTER = 1
+
+    _items = {}
+
+    def __init__(self, sh, helios_ip=None, update_cycle=60):
+        if helios_ip is None:
+            self.logger.error("Helios TCP: Configuration parameter helios_ip is required.")
+
+        self._sh = sh
+        self.logger = logging.getLogger(__name__)
+        self._helios_ip = helios_ip
+        self._client = ModbusTcpClient(self._helios_ip)
+        self.alive = False
+        self._is_connected = False
+        self._update_cycle = update_cycle
+
+
+    def run(self):
+        self._is_connected = self._client.connect()
+        if not self._is_connected:
+            self.logger.error("Helios TCP: Failed to connect to Modbus Server at {0}".format(self._helios_ip))
+        self._sh.scheduler.add('Helios TCP', self._update_values, prio=5, cycle=self._update_cycle)
+        self.alive = True
+
+
+    def stop(self):
+        self._client.close()
+        self.alive = False
+
+
+    def parse_item(self, item):
+        if 'helios_tcp' in item.conf:
+            varname = item.conf['helios_tcp']
+            if varname in VARLIST.keys():
+                self._items[varname] = item
+                return self.update_item
+            else:
+                self.logger.warning("Helios TCP: Ignoring unknown variable '{0}'".format(varname))
+
+    def _update_values(self):
+        for item in self._items:
+            self._read_value(self._items[item])
+
+
+    @staticmethod
+    def _string_to_registers(instr: str):
+        l = bytearray(instr, 'ascii')
+        return [k[0]*256 + k[1] for k in zip(l[::2], l[1::2])] + [0]
+
+
+    def _read_value(self, item):
+        try:
+            var = item.conf['helios_tcp']
+        except ValueError:
+            return
+
+        try:
+            varprop = VARLIST[var]
+        except KeyError:
+            self.logger.error("Helios TCP: Failed to find variable '{0}'".format(var))
+            return
+
+        # At first we write the variable name to read into the input registers:
+        payload = self._string_to_registers(varprop['var'])
+        request = self._client.write_registers(self.START_REGISTER, payload, unit=self.MODBUS_SLAVE)
+        if request is None:
+            self.logger.warning("Helios TCP: Failed to send read request for variable '{0}'".format(var))
+            return
+
+        # Now we may read the holding registers:
+        response = self._client.read_holding_registers(self.START_REGISTER, varprop['length'], unit=self.MODBUS_SLAVE)
+        if response is None:
+            self.logger.warning("Helios TCP: Failed to send read response for variable '{0}'".format(var))
+            return
+
+        # Now we may dedocde the result
+        # Note that we immediatly strip the varname from the result.
+        result = response.encode().decode('ascii')[8:]
+        result = list(result)
+
+        # Remove trailing zeros:
+        while result[-1] == '\x00':
+            result.pop()
+
+        result = ''.join(result)
+
+        # Finally we may cast the result and return the obtained value:
+        try:
+            item(varprop["type"](result))
+        except ValueError:
+            self.logger.warning("Helios TCP: Could not assign {0} to item {1}".format(varprop["type"](result), item.id()))
+            return
+
+
+    def update_item(self, item, caller=None, source=None, dest=None):
+        try:
+            var = item.conf['helios_tcp']
+        except ValueError:
+            return
+
+        newval = item()
+
+        try:
+            varprop = VARLIST[var]
+        except KeyError:
+            self.logger.error("Helios TCP: Failed to find variable '{0}'".format(var))
+            return None
+
+        if not varprop["write"]:
+            return None
+
+        if type(newval) != varprop["type"]:
+            self.logger.error("Helios TCP: Type mismatch for variable '{0}'".format(var))
+            return None
+
+        if newval < varprop["min"] or newval > varprop["max"]:
+            self.logger.error("Helios TCP: Variable '{0}' out of bounds. The allowed range is [{1}, {2}]".format(var, varprop["min"], varprop["max"]))
+            return None
+
+        if varprop["type"] == bool:
+            payload_string = "{0}={1}".format(varprop["var"], int(newval))
+        elif varprop["type"] == int:
+            payload_string = "{0}={1}".format(varprop["var"], int(newval))
+        elif varprop["type"] == float:
+            payload_string = "{0}={1:.1f}".format(varprop["var"], newval)
+        else:
+            self.logger.error("Helios TCP: Type {0} of varible '{1}' not known".format(varprop["type"], var))
+            return None
+
+        payload = self._string_to_registers(payload_string)
+        request = self._client.write_registers(self.START_REGISTER, payload, unit=self.MODBUS_SLAVE)
+        if request is None:
+            self.logger.warning("Helios TCP: Failed to send write request for variable '{0}'".format(var))
+            return None
+
