f72531eeffb73863c51c83e2a139ce5ae1a65e0c
bmxp <bernd.meiners@mail.de>
2020-01-05 09:27:20 +0100

    russound plugin now is a smart plugin

--- russound/README.md
+++ russound/README.md
@@ -2,7 +2,8 @@
 
 ## Requirements
 
-This plugin needs a Russound audio device in the network to communicate with. The communication protocol for the ethernet port has to be set to RIO.
+This plugin needs a Russound audio device in the network to communicate with. 
+The communication protocol for the ethernet port has to be set to RIO.
 
 ## Configuration
 
@@ -16,35 +17,52 @@ russound:
    # port: 9621
 ```
 
-This plugin talks by default with the Russound audio device with the given host ip on the default port 9621. If you have changed the default port you have to change it here as well.
+This plugin talks by default with the Russound audio device with the given host ip on the 
+default port 9621. 
+If you have changed the default port you have to change it here as well.
 
 ### items.yaml
-
 
 #### rus_path
 
 This attribute is mandatory. If you don't provide one the item will be ignored.
-The value must be given with the following format c.z.p where c is the number of the controller, z is the number of the zone and p is the system parameter of the Russound audio device like volume or treble.
+The value must be given with the following format ``c.z.p`` where 
+``c`` is the number of the controller,
+``z`` is the number of the zone and 
+``p`` is the system parameter of the Russound audio device like volume or treble.
 Right know the following russound parameter types are supported:
 
-* status = zone On/Off: type must be bool
-* volume = volume of the zone: type must be num [0..50]
-* bass = bass of the zone: type must be num [-10..10]
-* treble = treble of the zone: type must be num [-10..10]
-* balance = balance of the zone: type must be num [-10..10]
-* turnonvolume = volume on zone on: type must be num [0..50]
-* currentsource = number of the source: type must be num
-* mute = mute the zone volume: type must be bool
-* loudness = loudness of the zone: type must be bool
-* partymode = party mode setting of the zone: type must be string [ON/OFF/MASTER]
-* donotdisturb = do not disturb setting of the zone: type must be string
+* status = zone On/Off: item type must be bool
+* volume = volume of the zone: item type must be num [0..50]
+* bass = bass of the zone: item type must be num [-10..10]
+* treble = treble of the zone: item type must be num [-10..10]
+* balance = balance of the zone: item type must be num [-10..10]
+* turnonvolume = volume on zone on: item type must be num [0..50]
+* currentsource = number of the source: item type must be num
+* mute = mute the zone volume: item type must be bool
+* loudness = loudness of the zone: item type must be bool
+* partymode = party mode setting of the zone: item type must be string [ON/OFF/MASTER]
+* donotdisturb = do not disturb setting of the zone: item type must be string
 * name = name of the zone (readonly)
 
-For all parameters with specified ranges (volume, turnonvolume, bass, treble, balance) all values out of range are ignored and the minimum or maximum value will be taken. So if you set balance to 15 the plugin will send 10 to the Russound audio device.
+For all parameters with specified ranges (volume, turnonvolume, bass, treble, balance)
+all values out of range are ignored and the minimum or maximum value will be taken. 
+So if you set balance to ``15`` the plugin will send ``10`` to the Russound audio device.
 
-Besides the above parameters there are so called key codes. This are special values send like from a remote control which effects the current state of the Russound audio device itself or maybe a connected device if the Russound audio device can communicate with it. Look in the manual of your Russound which key codes are possible. If you specify a parameter not on the above list it will be interpreted as key code and send to the russound audio device as a "KeyRelease" event. This way you can e.g. define rus_path=1.5.channelup which will send the key code "channelup" to the russound. In case of the internal radio tuner this will change the current channel.
+Besides the above parameters there are so called key codes. This are special values send
+like from a remote control which effects the current state of the Russound audio device
+itself or maybe a connected device if the Russound audio device can communicate with it.
+Look in the manual of your Russound which key codes are possible. 
+If you specify a parameter not on the above list it will be interpreted as key code and 
+send to the russound audio device as a "KeyRelease" event. 
+This way you can e.g. define ``rus_path=1.5.channelup`` which will send the key code 
+"channelup" to the russound. In case of the internal radio tuner this will change the current channel.
 
-For such key codes it will be best to define the item with the aditional attribute enforce_updates=true so that the value of the item is not important. That means you can e.g. use a KNX push device that sends a ONE on every push to a group address and smarthome.py listens for that address. With this on every push the channelup key code is sent.
+For such key codes it will be best to define the item with the aditional attribute 
+``enforce_updates=true`` so that the value of the item is not important. 
+That means you can e.g. use a KNX push device that sends a ONE on every push 
+to a group address and SmartHomeNG listens for that address. 
+With this on every push the channelup key code is sent.
 
 #### Example
 
--- russound/__init__.py
+++ russound/__init__.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 # vim: set encoding=utf-8 tabstop=4 softtabstop=4 shiftwidth=4 expandtab
+#########################################################################
+#  Copyright 2012 Niko Will                     http://knx-user-forum.de/
+#  Copyright 2019-2020 Bernd Meiners                Bernd.Meiners@mail.de
+#########################################################################
+#  This file is part of SmartHomeNG.
 #
-# Copyright 2012 KNX-User-Forum e.V.            http://knx-user-forum.de/
-#
-#  This file is part of SmartHomeNG.    https://github.com/smarthomeNG//
+#  Russound plugin for network enabled Multiroom devices
 #
 #  SmartHomeNG is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -16,37 +19,92 @@
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with SmartHomeNG.  If not, see <http://www.gnu.org/licenses/>.
+#  along with SmartHomeNG. If not, see <http://www.gnu.org/licenses/>.
 #
+#########################################################################
 
-import logging
-
+from lib.module import Modules
+from lib.model.smartplugin import *
+from lib.item import Items
 import lib.connection
-
-logger = logging.getLogger('')
 
 REQ_DELIMITER = b'\r'
 RESP_DELIMITER = b'\r\n'
 
+class Russound(SmartPlugin,lib.connection.Client):
+    """
+    Main class of the Plugin. Does all plugin specific stuff and provides
+    the update functions for the items
+    """
 
-class Russound(lib.connection.Client):
+    PLUGIN_VERSION = '1.6.0'
 
-    def __init__(self, smarthome, host, port=9621):
-        lib.connection.Client.__init__(self, host, port, monitor=True)
+    def __init__(self, sh, *args, **kwargs):
+        """
+        Initalizes the plugin. The parameters describe for this method are pulled from the entry in plugin.conf.
+        """
+        from bin.smarthome import VERSION
+        if '.'.join(VERSION.split('.', 2)[:2]) <= '1.5':
+            self.logger = logging.getLogger(__name__)
+
+        try:
+            # sh = self.get_sh() to get it.
+            self.host = self.get_parameter_value('host')
+            self.port = self.get_parameter_value('port')
+            pass
+        except KeyError as e:
+            self.logger.critical(
+                "Plugin '{}': Inconsistent plugin (invalid metadata definition: {} not defined)".format(self.get_shortname(), e))
+            self._init_complete = False
+            return
+
+        # Initialization code goes here
+        lib.connection.Client.__init__(self, self.host, self.port, monitor=True)
         self.terminator = RESP_DELIMITER
-        self._sh = smarthome
         self.params = {}
         self.sources = {}
 
+        self.init_webinterface()
+        return
+
+    def run(self):
+        """
+        Run method for the plugin
+        """
+        self.logger.debug("Run method called")
+        self.alive = True
+        self.close()
+
+    def stop(self):
+        """
+        Stop method for the plugin
+        """
+        self.logger.debug("Stop method called")
+        self.alive = False
+
     def parse_item(self, item):
+        """
+        Default plugin parse_item method. Is called when the plugin is initialized.
+        The plugin can, corresponding to its attribute keywords, decide what to do with
+        the item in future, like adding it to an internal array for future reference
+        :param item:    The item to process.
+        :return:        If the plugin needs to be informed of an items change you should return a call back function
+                        like the function update_item down below. An example when this is needed is the knx plugin
+                        where parse_item returns the update_item function when the attribute knx_send is found.
+                        This means that when the items value is about to be updated, the call back function is called
+                        with the item, caller, source and dest as arguments and in case of the knx plugin the value
+                        can be sent to the knx with a knx write function within the knx plugin.
+        """
 #        if 'rus_src' in item.conf:
 #            s = int(item.conf['rus_src'])
 #            self.sources[s] = {'s': s, 'item':item}
 #            logger.debug("Source {0} added".format(s))
 #            return None
 
-        if 'rus_path' in item.conf:
-            path = item.conf['rus_path']
+        if self.has_iattr(item.conf, 'rus_path'):
+            self.logger.debug("parse item: {}".format(item))
+            
+            path = self.get_iattr_value(item.conf,'rus_path')
             parts = path.split('.', 2)
 
             if len(parts) is not 3:
@@ -59,22 +117,22 @@ class Russound(lib.connection.Client):
             param = parts[2]
 
         else:
-            if 'rus_controller' in item.conf:
-                c = item.conf['rus_controller']
+            if self.has_iattr(item.conf, 'rus_controller'):
+                c = self.get_iattr_value(item.conf,'rus_controller')
                 path = c + '.'
             else:
                 return None
 
-            if 'rus_zone' in item.conf:
-                z = item.conf['rus_zone']
+            if self.has_iattr(item.conf, 'rus_zone'):
+                z = self.get_iattr_value(item.conf,'rus_zone')
                 path += z + '.'
             else:
                 logger.warning(
                     "No zone specified for controller {0} in config of item {1}".format(c, item))
                 return None
 
-            if 'rus_parameter' in item.conf:
-                param = item.conf['rus_parameter']
+            if self.has_iattr(item.conf, 'rus_parameter'):
+                param = self.get_iattr_value(item.conf,'rus_parameter')
                 path += param
             else:
                 logger.warning(
@@ -82,9 +140,10 @@ class Russound(lib.connection.Client):
                 return None
 
             if param == 'relativevolume':
-                item._enforce_updates = True
+                #item._enforce_updates = True
+                item.property.enforce_updates = True
 
-            item.conf['rus_path'] = path
+            self.set_attr_value(item.conf,'rus_path',path)
 
         param = param.lower()
         self.params[path] = {'c':
@@ -104,41 +163,58 @@ class Russound(lib.connection.Client):
         return val
 
     def update_item(self, item, caller=None, source=None, dest=None):
-        if caller != 'Russound':
-            path = item.conf['rus_path']
-            p = self.params[path]
-            cmd = p['param']
-            c = p['c']
-            z = p['z']
+        """
+        Item has been updated
 
-            if cmd == 'bass':
-                self.send_set(c, z, cmd, self._restrict(item(), -10, 10))
-            elif cmd == 'treble':
-                self.send_set(c, z, cmd, self._restrict(item(), -10, 10))
-            elif cmd == 'balance':
-                self.send_set(c, z, cmd, self._restrict(item(), -10, 10))
-            elif cmd == 'loudness':
-                self.send_set(c, z, cmd, 'ON' if item() else 'OFF')
-            elif cmd == 'turnonvolume':
-                self.send_set(c, z, cmd, self._restrict(item(), 0, 50))
-            elif cmd == 'status':
-                self.send_event(c, z, 'ZoneOn' if item() else 'ZoneOff')
-            elif cmd == 'partymode':
-                self.send_event(c, z, cmd, item().lower())
-            elif cmd == 'donotdisturb':
-                self.send_event(c, z, cmd, 'on' if item() else 'off')
-            elif cmd == 'volume':
-                self.send_event(c, z, 'KeyPress', 'Volume',
-                                self._restrict(item(), 0, 50))
-            elif cmd == 'currentsource':
-                self.send_event(c, z, 'SelectSource', item())
-            elif cmd == 'relativevolume':
-                self.send_event(c, z, 'KeyPress',
-                                'VolumeUp' if item() else 'VolumeDown')
-            elif cmd == 'name':
-                return
-            else:
-                self.key_release(c, z, cmd)
+        This method is called, if the value of an item has been updated by SmartHomeNG.
+        It should write the changed value out to the device (hardware/interface) that
+        is managed by this plugin.
+
+        :param item: item to be updated towards the plugin
+        :param caller: if given it represents the callers name
+        :param source: if given it represents the source
+        :param dest: if given it represents the dest
+        """
+        if self.alive and caller != self.get_shortname():
+            # code to execute if the plugin is not stopped
+            # and only, if the item has not been changed by this this plugin:
+            self.logger.info("Update item: {}, item has been changed outside this plugin".format(item.id()))
+
+            if self.has_iattr(item.conf, 'rus_path'):
+                path = self.get_iattr_value(item.conf, 'rus_path')
+                p = self.params[path]
+                cmd = p['param']
+                c = p['c']
+                z = p['z']
+
+                if cmd == 'bass':
+                    self.send_set(c, z, cmd, self._restrict(item(), -10, 10))
+                elif cmd == 'treble':
+                    self.send_set(c, z, cmd, self._restrict(item(), -10, 10))
+                elif cmd == 'balance':
+                    self.send_set(c, z, cmd, self._restrict(item(), -10, 10))
+                elif cmd == 'loudness':
+                    self.send_set(c, z, cmd, 'ON' if item() else 'OFF')
+                elif cmd == 'turnonvolume':
+                    self.send_set(c, z, cmd, self._restrict(item(), 0, 50))
+                elif cmd == 'status':
+                    self.send_event(c, z, 'ZoneOn' if item() else 'ZoneOff')
+                elif cmd == 'partymode':
+                    self.send_event(c, z, cmd, item().lower())
+                elif cmd == 'donotdisturb':
+                    self.send_event(c, z, cmd, 'on' if item() else 'off')
+                elif cmd == 'volume':
+                    self.send_event(c, z, 'KeyPress', 'Volume',
+                                    self._restrict(item(), 0, 50))
+                elif cmd == 'currentsource':
+                    self.send_event(c, z, 'SelectSource', item())
+                elif cmd == 'relativevolume':
+                    self.send_event(c, z, 'KeyPress',
+                                    'VolumeUp' if item() else 'VolumeDown')
+                elif cmd == 'name':
+                    return
+                else:
+                    self.key_release(c, z, cmd)
 
     def send_set(self, c, z, cmd, value):
         self._send_cmd(
@@ -221,12 +297,11 @@ class Russound(lib.connection.Client):
 
     def _decode(self, cmd, value):
         cmd = cmd.lower()
-
-        if cmd == 'bass' or cmd == 'treble' or cmd == 'balance' or cmd == 'turnonvolume' or cmd == 'volume':
+        if cmd in ['bass','treble','balance','turnonvolume','volume']:
             return int(value)
-        elif cmd == 'loudness' or cmd == 'status' or cmd == 'mute':
+        elif cmd in ['loudness','status','mute']:
             return value == 'ON'
-        elif cmd == 'partymode' or cmd == 'donotdisturb':
+        elif cmd in ['partymode','donotdisturb']:
             return value.lower()
         elif cmd == 'currentsource':
             return value
@@ -249,9 +324,134 @@ class Russound(lib.connection.Client):
         for s in self.sources:
             self._watch_source(s)
 
-    def run(self):
-        self.alive = True
 
-    def stop(self):
-        self.alive = False
-        self.close()
+    def poll_device(self):
+        """
+        Polls for updates of the device
+
+        This method is only needed, if the device (hardware/interface) does not propagate
+        changes on it's own, but has to be polled to get the actual status.
+        It is called by the scheduler which is set within run() method.
+        """
+        # # get the value from the device
+        # device_value = ...
+        #
+        # # find the item(s) to update:
+        # for item in self.sh.find_items('...'):
+        #
+        #     # update the item by calling item(value, caller, source=None, dest=None)
+        #     # - value and caller must be specified, source and dest are optional
+        #     #
+        #     # The simple case:
+        #     item(device_value, self.get_shortname())
+        #     # if the plugin is a gateway plugin which may receive updates from several external sources,
+        #     # the source should be included when updating the the value:
+        #     item(device_value, self.get_shortname(), source=device_source_id)
+        pass
+
+    def init_webinterface(self):
+        """"
+        Initialize the web interface for this plugin
+
+        This method is only needed if the plugin is implementing a web interface
+        """
+        try:
+            self.mod_http = Modules.get_instance().get_module(
+                'http')  # try/except to handle running in a core version that does not support modules
+        except:
+            self.mod_http = None
+        if self.mod_http == None:
+            self.logger.error("Not initializing the web interface")
+            return False
+
+        import sys
+        if not "SmartPluginWebIf" in list(sys.modules['lib.model.smartplugin'].__dict__):
+            self.logger.warning("Web interface needs SmartHomeNG v1.5 and up. Not initializing the web interface")
+            return False
+
+        # set application configuration for cherrypy
+        webif_dir = self.path_join(self.get_plugin_dir(), 'webif')
+        config = {
+            '/': {
+                'tools.staticdir.root': webif_dir,
+            },
+            '/static': {
+                'tools.staticdir.on': True,
+                'tools.staticdir.dir': 'static'
+            }
+        }
+
+        # Register the web interface as a cherrypy app
+        self.mod_http.register_webif(WebInterface(webif_dir, self),
+                                     self.get_shortname(),
+                                     config,
+                                     self.get_classname(), self.get_instance_name(),
+                                     description='')
+
+        return True
+
+
+# ------------------------------------------
+#    Webinterface of the plugin
+# ------------------------------------------
+
+import cherrypy
+from jinja2 import Environment, FileSystemLoader
+
+
+class WebInterface(SmartPluginWebIf):
+
+    def __init__(self, webif_dir, plugin):
+        """
+        Initialization of instance of class WebInterface
+
+        :param webif_dir: directory where the webinterface of the plugin resides
+        :param plugin: instance of the plugin
+        :type webif_dir: str
+        :type plugin: object
+        """
+        self.logger = logging.getLogger(__name__)
+        self.webif_dir = webif_dir
+        self.plugin = plugin
+        self.tplenv = self.init_template_environment()
+
+        self.items = Items.get_instance()
+
+    @cherrypy.expose
+    def index(self, reload=None):
+        """
+        Build index.html for cherrypy
+
+        Render the template and return the html file to be delivered to the browser
+
+        :return: contents of the template after beeing rendered
+        """
+        tmpl = self.tplenv.get_template('index.html')
+        # add values to be passed to the Jinja2 template eg: tmpl.render(p=self.plugin, interface=interface, ...)
+        return tmpl.render(p=self.plugin, items=sorted(self.items.return_items(), key=lambda k: str.lower(k['_path'])))
+
+
+    @cherrypy.expose
+    def get_data_html(self, dataSet=None):
+        """
+        Return data to update the webpage
+
+        For the standard update mechanism of the web interface, the dataSet to return the data for is None
+
+        :param dataSet: Dataset for which the data should be returned (standard: None)
+        :return: dict with the data needed to update the web page.
+        """
+        if dataSet is None:
+            # get the new data
+            data = {}
+
+            # data['item'] = {}
+            # for i in self.plugin.items:
+            #     data['item'][i]['value'] = self.plugin.getitemvalue(i)
+            #
+            # return it as json the the web page
+            # try:
+            #     return json.dumps(data)
+            # except Exception as e:
+            #     self.logger.error("get_data_html exception: {}".format(e))
+        return {}
