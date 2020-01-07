d1c785a737658c22b1ffc9b7b5f0b4dd8fd4bc1e
psilo909 <rene.friess@gmx.de>
2020-01-07 19:05:27 +0100

    AVM: upgraded version

--- avm/__init__.py
+++ avm/__init__.py
@@ -511,7 +511,7 @@ class AVM(SmartPlugin):
     Main class of the Plugin. Does all plugin specific stuff and provides the update functions for the different TR-064 services on the FritzDevice
     """
 
-    PLUGIN_VERSION = "1.5.7"
+    PLUGIN_VERSION = "1.5.8"
 
     _header = {'SOAPACTION': '', 'CONTENT-TYPE': 'text/xml; charset="utf-8"'}
     _envelope = """
