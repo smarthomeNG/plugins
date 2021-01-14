29e11605641d0d63cdd706258634c9fcab38507e
Onkel Andy <onkelandy@hotmail.com>
2021-01-14 20:52:50 +0100

    stateengine plugin: se_use can now be a list and be defined by item, eval, struct or value

--- stateengine/StateEngineState.py
+++ stateengine/StateEngineState.py
@@ -18,10 +18,14 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this plugin. If not, see <http://www.gnu.org/licenses/>.
 #########################################################################
+from lib.item.item import Item
 from . import StateEngineTools
 from . import StateEngineConditionSets
 from . import StateEngineActions
 from . import StateEngineValue
+from . import StateEngineStruct
+from lib.item import Items
+import copy
 
 
 # Class representing an object state, consisting of name, conditions to be met and configured actions for state
@@ -56,7 +60,9 @@ class SeState(StateEngineTools.SeItemChild):
     # item_state: item containing configuration of state
     def __init__(self, abitem, item_state):
         super().__init__(abitem)
+        self.itemsApi = Items.get_instance()
         self.__item = item_state
+        self.__itemClass = Item
         try:
             self.__id = self.__item.property.path
             self._log_info("Init state {}", self.__id)
@@ -64,6 +70,8 @@ class SeState(StateEngineTools.SeItemChild):
             self._log_info("Problem init state ID of Item {}. {}", self.__item, err)
         self.__name = ""
         self.__text = StateEngineValue.SeValue(self._abitem, "State Name", False, "str")
+        self.__use = StateEngineValue.SeValue(self._abitem, "State configuration extension", True, "item")
+        self.__use_done = []
         self.__conditions = StateEngineConditionSets.SeConditionSets(self._abitem)
         self.__actions_enter_or_stay = StateEngineActions.SeActions(self._abitem)
         self.__actions_enter = StateEngineActions.SeActions(self._abitem)
@@ -109,6 +117,7 @@ class SeState(StateEngineTools.SeItemChild):
         self._log_info("Finished Web Interface Update")
         self._log_info("Name: {0}", self.name)
         self.__text.write_to_logger()
+        self.__use.write_to_logger()
         if self.__conditions.count() > 0:
             self._log_info("Condition sets to enter state:")
             self._log_increase_indent()
@@ -201,48 +210,86 @@ class SeState(StateEngineTools.SeItemChild):
     # recursion_depth: current recursion_depth (recursion is canceled after five levels)
     # item_stateengine: StateEngine-Item defining items for conditions
     # abitem_object: Related SeItem instance for later determination of current age and current delay
-    def __fill(self, item_state, recursion_depth):
+    def __fill(self, item_state, recursion_depth, se_use=None):
         if recursion_depth > 5:
             self._log_error("{0}/{1}: too many levels of 'use'", self.id, item_state.property.path)
             return
-
         # Import data from other item if attribute "use" is found
         if "se_use" in item_state.conf:
-            item_state.expand_relativepathes('se_use', '', '')
-            use_item = self._abitem.return_item(item_state.conf["se_use"])
-            if use_item is not None:
-                self.__fill(use_item, recursion_depth + 1)
+            self.__use.set_from_attr(item_state, "se_use")
+            _use = self.__use.get()
+            if self.__use.is_empty() or _use is None:
+                self._log_warning("se_use is set up in a wrong way - ignoring {}", _use)
             else:
-                self._log_error("{0}: Referenced item '{1}' not found!", item_state.property.path, item_state.conf["se_use"])
-            self._log_info("{0}: Reading se_use {1}. Nevertheless, it is recommended to use struct items instead.",
-                           item_state.property.path, item_state.conf["se_use"])
+                _use = [_use] if not isinstance(_use, list) else StateEngineTools.flatten_list(_use)
+
+                for loop, element in enumerate(_use):
+                    try:
+                        _name = element.property.path
+                    except Exception:
+                        _name = element
+                    _fill = True
+                    _path = None
+                    if isinstance(element, StateEngineStruct.SeStruct):
+                        _path = element.property.path
+                        text1 = "Reading struct {0}. It is{1} a valid struct for the state configuration.{2}"
+                        _fill = element.property.valid_se_use
+                        valid1 = " NOT" if _fill is False else ""
+                        valid2 = " Ignoring." if _fill is False else ""
+                        self._log_info(text1, _path, valid1, valid2)
+                    elif isinstance(element, self.__itemClass):
+                        _path = element.property.path
+                        text1 = "Reading Item {0}. It is{1} a valid item for the state configuration.{2}"
+                        valid1 = " NOT" if _fill is False else " most likely"
+                        valid2 = " Ignoring." if _fill is False else ""
+                        self._log_info(text1, _path, valid1, valid2)
+
+                    if _path is None:
+                        if not isinstance(element, str):
+                            self._log_warning("se_use {} is not valid.", element)
+                    elif _fill and element not in self.__use_done:
+                        #self._log_debug("Adding {} again to state fill function.", _name)
+                        self.__use_done.append(element)
+                        self.__fill(element, recursion_depth + 1, _name)
 
         # Get action sets and condition sets
         parent_item = item_state.return_parent()
         child_items = item_state.return_children()
+        _conditioncount = 0
+        _enter_actioncount = 0
+        _enter_stay_actioncount = 0
+        _leave_actioncount = 0
+        _stay_actioncount = 0
         for child_item in child_items:
             child_name = StateEngineTools.get_last_part_of_item_id(child_item)
             try:
                 if child_name == "on_enter":
                     for attribute in child_item.conf:
+                        _enter_actioncount += 1
                         self.__actions_enter.update(attribute, child_item.conf[attribute])
                 elif child_name == "on_stay":
                     for attribute in child_item.conf:
+                        _stay_actioncount += 1
                         self.__actions_stay.update(attribute, child_item.conf[attribute])
                 elif child_name == "on_enter_or_stay":
                     for attribute in child_item.conf:
+                        _enter_stay_actioncount += 1
                         self.__actions_enter_or_stay.update(attribute, child_item.conf[attribute])
                 elif child_name == "on_leave":
                     for attribute in child_item.conf:
+                        _leave_actioncount += 1
                         self.__actions_leave.update(attribute, child_item.conf[attribute])
                 elif child_name == "enter" or child_name.startswith("enter_"):
+                    _conditioncount += 1
                     self.__conditions.update(child_name, child_item, parent_item)
             except ValueError as ex:
                 raise ValueError("Condition {0} error: {1}".format(child_name, ex))
 
         # Actions defined directly in the item go to "enter_or_stay"
         for attribute in item_state.conf:
-            self.__actions_enter_or_stay.update(attribute, item_state.conf[attribute])
+            _enter_stay_actioncount += self.__actions_enter_or_stay.update(attribute, item_state.conf[attribute]) or 0
+
+        _total_actioncount = _enter_actioncount + _stay_actioncount + _enter_stay_actioncount + _leave_actioncount
 
         # if an item name is given, or if we do not have a name after returning from all recursions,
         # use item name as state name
@@ -261,3 +308,13 @@ class SeState(StateEngineTools.SeItemChild):
             self.__actions_stay.complete(item_state)
             self.__actions_enter_or_stay.complete(item_state)
             self.__actions_leave.complete(item_state)
+
+        _summary = "{} on_enter, {} on_stay , {} on_enter_or_stay, {} on_leave"
+        if se_use is not None:
+            self._log_debug("Added {} action(s) based on se_use {}. " + _summary, _total_actioncount, se_use,
+                           _enter_actioncount, _stay_actioncount, _enter_stay_actioncount, _leave_actioncount)
+            self._log_debug("Added {} condition set(s) based on se_use: {}", _conditioncount, se_use)
+        else:
+            self._log_debug("Added {} action(s) based on item configuration: " + _summary, _total_actioncount,
+                           _enter_actioncount, _stay_actioncount, _enter_stay_actioncount, _leave_actioncount)
+            self._log_debug("Added {} condition set(s) based on item configuration", _conditioncount)
