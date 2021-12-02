07777dece6c02a430ee5089ebcaa5f728f243a2e
msinn <msinn@users.noreply.github.com>
2021-12-02 23:38:42 +0100

    bin.locale.yaml: Fix for identation

--- smartvisu/__init__.py
+++ smartvisu/__init__.py
@@ -45,7 +45,7 @@ from .svinstallwidgets import SmartVisuInstallWidgets
 #########################################################################
 
 class SmartVisu(SmartPlugin):
-    PLUGIN_VERSION="1.8.3"
+    PLUGIN_VERSION="1.8.3a"
     ALLOW_MULTIINSTANCE = True
 
     visu_definition = None
