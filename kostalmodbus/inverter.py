74048f201e01e42ef2d13f70da6b7076291f1654
Thomas <beckerth@gmx.net>
2022-11-06 16:52:04 +0100

    Update inverter.py

--- kostalmodbus/inverter.py
+++ kostalmodbus/inverter.py
@@ -7,7 +7,7 @@ import logging
 import pymodbus
 
 from lib.model.smartplugin import *
-from pymodbus.client.sync import ModbusTcpClient
+from pymodbus.client import ModbusTcpClient
 from pymodbus.payload import BinaryPayloadDecoder
 from pymodbus.constants import Endian
 
@@ -43,27 +43,27 @@ class Inverter:
         return self.registers
 
     def __read_float(self, adr_dec):
-        result = self.client.read_holding_registers(adr_dec, 2, unit=71)
+        result = self.client.read_holding_registers(adr_dec, 2, slave=71)
         float_value = BinaryPayloadDecoder.fromRegisters(result.registers,byteorder=Endian.Big,wordorder=Endian.Little)
         return round(float_value.decode_32bit_float(), 2)
 
     def __read_u16(self, adr_dec):
-        result = self.client.read_holding_registers(adr_dec, 1, unit=71)
+        result = self.client.read_holding_registers(adr_dec, 1, slave=71)
         u16_value = BinaryPayloadDecoder.fromRegisters(result.registers, byteorder=Endian.Big,wordorder=Endian.Little)
         return u16_value.decode_16bit_uint()
 
     def __read_u16_2(self, adr_dec):
-        result = self.client.read_holding_registers(adr_dec, 2, unit=71)
+        result = self.client.read_holding_registers(adr_dec, 2, slave=71)
         u16_2_value = BinaryPayloadDecoder.fromRegisters(result.registers, byteorder=Endian.Big,wordorder=Endian.Little)
         return u16_2_value.decode_16bit_uint()
 
     def __read_u32(self, adr_dec):
-        result = self.client.read_holding_registers(adr_dec, 2, unit=71)
+        result = self.client.read_holding_registers(adr_dec, 2, slave=71)
         u32_value = BinaryPayloadDecoder.fromRegisters(result.registers, byteorder=Endian.Big,wordorder=Endian.Little)
         return u32_value.decode_32bit_uint()
 
     def __read_s16(self, adr_dec):
-        result = self.client.read_holding_registers(adr_dec, 1, unit=71)
+        result = self.client.read_holding_registers(adr_dec, 1, slave=71)
         s16_value = BinaryPayloadDecoder.fromRegisters(result.registers, byteorder=Endian.Big,wordorder=Endian.Little)
         return s16_value.decode_16bit_uint()
 
