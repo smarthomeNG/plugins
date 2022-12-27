e77bdfabb212c379a5a0df351e6055995adc2132
Hasenradball <mail@frankhaefele.de>
2022-12-27 12:57:08 +0100

    rework of rcswitch plugin up to v2.0.0

--- rcswitch/__init__.py
+++ rcswitch/__init__.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 # vim: set encoding=utf-8 tabstop=4 softtabstop=4 shiftwidth=4 expandtab
 #########################################################################
-# Copyright 2017      Daniel Frank                knx-user-forum.de:dafra 
+#  Copyright 2022      Frank Häfele                  mail@frankhaefele.de
 #########################################################################
-#  This file is part of SmartHomeNG.    https://github.com/smarthomeNG//
+#  This file is part of SmartHomeNG.
+#  https://www.smarthomeNG.de
+#  https://knx-user-forum.de/forum/supportforen/smarthome-py
+#
+#  Sample plugin for new plugins to run with SmartHomeNG version 1.8 and
+#  upwards.
 #
 #  SmartHomeNG is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -17,94 +22,71 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with SmartHomeNG. If not, see <http://www.gnu.org/licenses/>.
+#
 #########################################################################
 
-import logging
 from lib.model.smartplugin import SmartPlugin
-from datetime import datetime, timedelta
-from socket import gethostname
+from lib.item import Items
+from .cRcSocketSwitch import cRcSocketSwitch
 import time
 import threading
-import subprocess
-import os
-from subprocess import DEVNULL
-import shlex
 
-class RCswitch(SmartPlugin):
+
+class rcSwitch_python(SmartPlugin):
 
 	ALLOW_MULTIINSTANCE = False
-	PLUGIN_VERSION = "1.2.2"
+	PLUGIN_VERSION = "2.0.0"
 
-	def __init__(self, smarthome, rcswitch_dir='/usr/local/bin/rcswitch-pi', rcswitch_sendDuration='0.5', rcswitch_host=None, rcswitch_user=None, rcswitch_password=None):
-		self.logger = logging.getLogger(__name__)
-		self.setupOK = True
-		self.mapping = {'a':1,'A':1,'b':2,'B':2,'c':3,'C':3,'d':4,'D':4,'e':5,'E':5}
-		self._sh = smarthome
-		
-		# format path: cut possible '/' at end of rcswitch_dir parameter
-		if rcswitch_dir[len(rcswitch_dir)-1] == '/':
-			self.rcswitch_dir = rcswitch_dir[0:len(rcswitch_dir)-1]
-		else:
-			self.rcswitch_dir = rcswitch_dir
+	def __init__(self, sh):
+		"""
+		Initalizes the plugin.
 
-		# Check optional Parameters: check sendDuration
-		try:
-			self.sendDuration = float(rcswitch_sendDuration)
-		except Exception as e:
-			self.sendDuration = float(0.5)
-			self.logger.warning('RCswitch: Argument {} for rcswitch_sendDuration is not a valid number. Using default value instead.'.format(rcswitch_sendDuration))
-		
-		# Handle host, check if anything is defined in rcswitch_host parameter and if is valid hostname or valid IPv4 adress
-		if (rcswitch_host and (self.is_hostname(rcswitch_host) or self.is_ipv4(rcswitch_host))):
-			# then check if user defined its own local host -> error
-			if ((rcswitch_host == gethostname()) or (rcswitch_host == '127.0.0.1')):
-				self.logger.error('RCswitch: rcswitch_host is defined as your own machine, not the remote address! Please check the parameter rcswitch_host, >>{}<< it seems to be not correct!'.format(rcswitch_host))
+		If you need the sh object at all, use the method self.get_sh() to get it. There should be almost no need for
+		a reference to the sh object any more.
 
-			#check connection to remote host and accept fingerprint
-			try:
-				# following line shall raise an error in case connection is not possible. 
-				self.user = subprocess.check_output(shlex.split('sshpass -p {} ssh -o StrictHostKeyChecking=no {}@{} grep {} /etc/passwd'.format(rcswitch_password, rcswitch_user, rcswitch_host, rcswitch_user)), stderr=DEVNULL).decode('utf8')[0:len(rcswitch_user)]
-				# check  if rc switch is installed at the specified path on remote host
-				self.fileStat = subprocess.check_output(shlex.split('sshpass -p {} ssh {}@{} stat -c %a {}'.format(rcswitch_password, rcswitch_user, rcswitch_host, self.rcswitch_dir)), stderr=DEVNULL).decode('utf8')
-				self.rcswitch_dir = ('sshpass -p {} ssh {}@{} {}'.format(rcswitch_password, rcswitch_user, rcswitch_host, self.rcswitch_dir))
-				self.logger.info('RCswitch: Using {} as host.'.format(rcswitch_host))
-			except subprocess.CalledProcessError as e:
-				self.setupOK = False
-				# give user hint where the problem is located
-				try:
-					if (user == rcswitch_user):
-						self.logger.error('RCswitch: send file of RCswitch not found at {} on {}. Check if RCswitch is installed correctly at specifed path on {}. System returned: {}'.format(self.rcswitch_dir, rcswitch_host, rcswitch_host, e))
-					else:
-						self.logger.error('RCswitch: send file of RCswitch not found at {} on {}. Additional problem with user authentication possible. System returned: {}'.format(self.rcswitch_dir, rcswitch_host, e))
-				except UnboundLocalError as e:
-					self.logger.error('RCswitch: Cannot connect to {}. Check rcswitch_host, rcswitch_user and rcswitch_password are set (correctly). Ensure SSH server is running on {}. System returned: {}'.format(rcswitch_host, rcswitch_host, e))
-		else:
-			# check if rc switch is installed at the specified path
-			if not os.path.isfile('{}/send'.format(self.rcswitch_dir)):
-				self.logger.error('RCswitch: send file of RCswitch not found at {} on localhost. Check path, if RCswitch is installed correctly on target, and correct format of v4 IP adress (in case rcswitch_host is defined).'.format(self.rcswitch_dir))
-				self.setupOK = False
-			else:
-				self.logger.info('RCswitch: setup on localhost OK')
-		
+		Plugins have to use the new way of getting parameter values:
+		use the SmartPlugin method get_parameter_value(parameter_name). Anywhere within the Plugin you can get
+		the configured (and checked) value for a parameter by calling self.get_parameter_value(parameter_name). It
+		returns the value in the datatype that is defined in the metadata.
+		"""
+		# Call init code of parent class (SmartPlugin)
+		super().__init__()
+		# Initialization code goes here
+		# internal member variables
+		self._gpio = int(self.get_parameter_value('rcswitch_gpio'))
+		self._send_duration = float(self.get_parameter_value('rcswitch_sendDuration'))
 		# setup semaphore
 		self.lock = threading.Lock()
 		
+		# On initialization error use:
+		#   self._init_complete = False
+		#   return
+
+		# if plugin should start even without web interface
+		#self.init_webinterface(WebInterface)
+		# if plugin should not start without web interface
+		# if not self.init_webinterface():
+		#     self._init_complete = False
+		return
+		
 	def run(self):
+		self.logger.debug("Run method called")
 		self.alive = True
 
 	def stop(self):
+		self.logger.debug("Stop method called")
 		self.alive = False
 
 	def parse_item(self, item):
 		# generate warnings for incomplete configured itemns
-		if self.has_iattr(item.conf, 'rc_device'):
-			if self.has_iattr(item.conf, 'rc_code'):
+		if self.has_iattr(item.conf, 'rc_SystemCode') or self.has_iattr(item.conf, 'rc_code'):
+			if self.has_iattr(item.conf, 'rc_ButtonCode') or self.has_iattr(item.conf, 'rc_device'):
 				return self.update_item
 			else:
-				self.logger.warning('RC Switch: attribute rc_code for {} missing. Item will be ignored by RCswitch plugin'.format(item))
+				self.logger.warning('Warning: attribute <rc_ButtonCode>/<rc_device> for {} missing. Item will be ignored by rcSwitch_python plugin'.format(item))
 				return None
-		elif self.has_iattr(item.conf, 'rc_code'): 
-			self.logger.warning('RC Switch: attribute rc_device for {} missing. Item will be ignored by RCswitch plugin'.format(item))
+		elif self.has_iattr(item.conf, 'rc_ButtonCode') or self.has_iattr(item.conf, 'rc_device'): 
+			self.logger.warning('Warning: attribute <rc_SystemCode>/<rc_code> for {} missing. Item will be ignored by RCswitch plugin'.format(item))
 			return None
 		else:
 			return None
@@ -112,23 +94,37 @@ class RCswitch(SmartPlugin):
 
 	def update_item(self, item, caller=None, source=None, dest=None):
 		# send commands to devices
-		if self.has_iattr(item.conf, 'rc_code') and self.has_iattr(item.conf, 'rc_device') and self.setupOK: #if 'rc_device' in item.conf and 'rc_code' in item.conf and self.setupOK:
+		# if 'rc_ButtonCode' in item.conf and 'rc_SystemCode' in item.conf and self.setupOK:
+		if self.alive and caller != self.get_shortname():
+
+		# if SystemCode and Buttoncode used
+			if self.has_iattr(item.conf, 'rc_SystemCode') and self.has_iattr(item.conf, 'rc_ButtonCode'):
+				SystemCode = self.get_iattr_value(item.conf, 'rc_SystemCode')
+				ButtonCode = self.get_iattr_value(item.conf, 'rc_ButtonCode')
+
+			# if rc_code and rc_device used
+			elif self.has_iattr(item.conf, 'rc_code') and self.has_iattr(item.conf, 'rc_device'):
+				SystemCode = self.get_iattr_value(item.conf, 'rc_code')
+				ButtonCode = self.get_iattr_value(item.conf, 'rc_device')
+
+			self.logger.info(f"update_item was called with item {item.property.path} from caller {caller}, source {source} and dest {dest}")
 			# prepare parameters
-			value = item()
-			rcCode = self.get_iattr_value(item.conf, 'rc_code') 
-			rcDevice = self.get_iattr_value(item.conf, 'rc_device')
-		
+			value = int(item())
+			values = (SystemCode, ButtonCode, value)
+			
 			# avoid parallel access by use of semaphore
 			self.lock.acquire()
 			# sending commands
-			if(rcDevice in self.mapping):#handling of device encoded with a,A,b,...
-				subprocess.call(shlex.split('{}/send {} {} {}'.format(self.rcswitch_dir, rcCode, self.mapping[rcDevice], int(value))), stdout=DEVNULL, stderr=DEVNULL)
-				self.logger.info('RC Switch: setting device {} with system code {} to {}'.format(rcDevice, rcCode, value))
+			try:
+				# create Brennenstuhl RCS1000N object 
+				obj = cRcSocketSwitch.RCS1000N(self._gpio)
+				# prepare and send values
+				obj.send(*values)
+			except Exception as err:
+				self.logger.error('Error: during instantiation of object or during send to device: {}'.format(err))
 			else:
-				try:#handling of devices encoded with 1,2,3
-					subprocess.call(shlex.split('{}/send {} {} {}'.format(self.rcswitch_dir, rcCode, int(rcDevice), int(value))), stdout=DEVNULL, stderr=DEVNULL)
-					self.logger.info('RC Switch: setting device {} with system code {} to {}'.format(rcDevice, rcCode, value))
-				except Exception as e:#invalid encoding of device
-					self.logger.error('RC Switch: requesting invalid device {} with system code {} '.format(rcDevice, rcCode))
-			time.sleep(min(self.sendDuration,10))# give the transmitter time to complete sending of the command (but not more than 10s)
-			self.lock.release()
+				self.logger.info('Info: setting Device {} with SystemCode {} to {}'.format(ButtonCode, SystemCode, value))
+			finally:
+				# give the transmitter time to complete sending of the command (but not more than 10s)
+				time.sleep(min(self._send_duration, 10))
+				self.lock.release()
---
+++ rcswitch/user_doc.rst
@@ -0,0 +1,134 @@
+.. index:: Plugins; rcswitch (Brennenstuhl RCS 1000 N)
+.. index:: rcswitch
+
+================
+Plugins/rcswitch
+================
+
+
+Anforderungen
+-------------
+Das rcswitch Plugin beötigt folgenden Anforderungen an Hanrdware- und Software-Komponenten.
+
+Notwendige Hardware
+~~~~~~~~~~~~~~~~~~~
+
+* Raspberry Pi
+* 433 MHz Sende-Module
+* schaltbare Steckdose über 433 MHz (z.B.: Brennenstuhl RCS 1000 N)
+
+Notwendige Software
+~~~~~~~~~~~~~~~~~~~
+
+* die python Library `rpi-rf`
+
+
+Unterstützte Geräte
+~~~~~~~~~~~~~~~~~~~
+
+* Brennenstuhl RCS 1000 N
+
+
+Konfiguration
+-------------
+
+plugin.yaml
+~~~~~~~~~~~
+
+Bitte die Dokumentation lesen, die aus den Metadaten der plugin.yaml erzeugt wurde.
+
+Hier ein Beispiel wie das Plugin konfiguriert werden kann.
+
+.. code-block:: yaml
+
+    rcSwitchpython:
+        plugin_name: rcswitch
+        rcswitch_gpio: '17'
+        rcswitch_sendDuration: '0.5'
+
+    
+
+items.yaml
+~~~~~~~~~~
+
+Bitte die Dokumentation lesen, die aus den Metadaten der plugin.yaml erzeugt wurde.
+
+Hier ein Beispiel wie das Item konfiguriert werden kann.
+
+.. code-block:: yaml
+
+    Schaltsteckdose1:
+            name: Funkstedose 1 Wohnen
+            remark: Brennenstuhl RCS 1000 N
+            schalten:
+                type: bool
+                #rc_device: 'A'
+                #rc_code: 11010
+                rc_SystemCode: 11010
+                rc_ButtonCode: 'A'
+                visu_acl: rw
+                struct: uzsu.child
+
+
+.. image:: pictures/RCS1000N_switches.png
+  :width: 300
+  :alt: Dip Schalter der RCS 1000 N
+
+
+logic.yaml
+~~~~~~~~~~
+
+Bitte die Dokumentation lesen, die aus den Metadaten der plugin.yaml erzeugt wurde.
+
+
+Funktionen
+~~~~~~~~~~
+
+Bitte die Dokumentation lesen, die aus den Metadaten der plugin.yaml erzeugt wurde.
+
+
+Beispiele
+---------
+
+Aktuell keine weiteren Beispiele notwendig.
+
+Web Interface
+-------------
+
+Aktuell beitzt dieses Plugin kein Web Interface.
+
+
+Version History
+---------------
+
+v0.1
+~~~~~~
+
+* initial version. Supports sending on local machine
+
+v0.2
+~~~~~~
+
+* support of remote transmitter
+* more detailed failure report in case of misconfiguration
+* usage of subprocess module instead of the os module
+* support of literal device numbers
+
+v0.3
+~~~~~~
+
+* add import os at the init
+
+v1.2.4
+~~~~~~
+
+* add hostname support
+
+
+v2.0.0
+~~~~~~
+
+* remove wiringPi dependency due to availability reasons with bulleye
+* useage of python based GPIO Library `rpi-rf`
+* added `cRcSocketSwitch` class
+* rework of `__init__.py` to work with `rpi-rf`
