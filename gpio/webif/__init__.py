5d68d86034f663cd77a01a5fd7bcd3da6ca08aad
Onkel Andy <onkelandy@hotmail.com>
2023-01-30 23:17:18 +0100

    gpio plugin: fix minor typos, add user doc, implement webif_pagelength and correct automatic page length on browser window resize

--- gpio/webif/__init__.py
+++ gpio/webif/__init__.py
@@ -70,11 +70,20 @@ class WebInterface(SmartPluginWebIf):
 
         :return: contents of the template after beeing rendered
         '''
-        # item = self.plugin.get_sh().return_item(item_path)
+        global_pagelength = cherrypy.config.get("webif_pagelength")
+        if global_pagelength:
+            pagelength = global_pagelength
+            self.logger.debug("Global pagelength {}".format(pagelength))
+        try:
+            pagelength = self.plugin.webif_pagelength
+            self.logger.debug("Plugin pagelength {}".format(pagelength))
+        except Exception:
+            pass
 
         tmpl = self.tplenv.get_template('index.html')
         # add values to be passed to the Jinja2 template eg: tmpl.render(p=self.plugin, interface=interface, ...)
         return tmpl.render(p=self.plugin,
+                           webif_pagelength=pagelength,
                            language=self.plugin._sh.get_defaultlanguage(), now=self.plugin.shtime.now())
 
     @cherrypy.expose
