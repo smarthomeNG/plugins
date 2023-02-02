e046f55bbe260b3019c53bb538950f175ca0312c
Onkel Andy <onkelandy@hotmail.com>
2023-02-02 22:22:16 +0100

    mqtt plugin: adjust webif_pagelength handling, minor fixes in user_doc and webif

--- mqtt/webif/__init__.py
+++ mqtt/webif/__init__.py
@@ -72,20 +72,7 @@ class WebInterface(SmartPluginWebIf):
         :return: contents of the template after beeing rendered
         """
         self.plugin.get_broker_info()
-
-        global_pagelength = cherrypy.config.get("webif_pagelength")
-        if global_pagelength:
-            pagelength = global_pagelength
-            self.logger.debug("Global pagelength {}".format(pagelength))
-        else:
-            pagelength = 100
-            self.logger.debug("Default pagelength {}".format(pagelength))
-        # try to get the webif pagelength from the plugin specific plugin.yaml configuration
-        try:
-            pagelength = self.plugin.webif_pagelength
-            self.logger.debug("Plugin pagelength {}".format(pagelength))
-        except Exception:
-            pass
+        pagelength = self.plugin.get_parameter_value('webif_pagelength')
         tmpl = self.tplenv.get_template('index.html')
         # add values to be passed to the Jinja2 template eg: tmpl.render(p=self.plugin, interface=interface, ...)
         return tmpl.render(p=self.plugin,
