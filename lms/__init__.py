30498efb842704b270f59268f7648b9046bc56fe
Onkel Andy <onkelandy@hotmail.com>
2023-07-01 01:02:08 +0200

    LMS Plugin: fix SDP_standalone issue

--- lms/__init__.py
+++ lms/__init__.py
@@ -26,6 +26,7 @@ import os
 import sys
 
 if __name__ == '__main__':
+    builtins.SDP_standalone = True
 
     class SmartPlugin():
         pass
@@ -36,6 +37,8 @@ if __name__ == '__main__':
     BASE = os.path.sep.join(os.path.realpath(__file__).split(os.path.sep)[:-3])
     sys.path.insert(0, BASE)
 
+else:
+    builtins.SDP_standalone = False
 
 from lib.model.sdp.globals import (CUSTOM_SEP, PLUGIN_ATTR_NET_HOST, PLUGIN_ATTR_RECURSIVE, PLUGIN_ATTR_CONN_TERMINATOR)
 from lib.model.smartdeviceplugin import SmartDevicePlugin, Standalone
