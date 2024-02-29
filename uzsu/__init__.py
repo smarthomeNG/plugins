ae9e0359b7a40f2dca8ad40ef4225f08e381e738
Onkel Andy <onkelandy@hotmail.com>
2024-02-29 16:19:40 +0100

    uzsu plugin: bump version to 2.0.1

--- uzsu/__init__.py
+++ uzsu/__init__.py
@@ -101,7 +101,7 @@ class UZSU(SmartPlugin):
 
     ALLOW_MULTIINSTANCE = False
 
-    PLUGIN_VERSION = "2.0.0"      # item buffer for all uzsu enabled items
+    PLUGIN_VERSION = "2.0.1"      # item buffer for all uzsu enabled items
 
     def __init__(self, smarthome):
         """
