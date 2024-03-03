ddbf3e74e523c48ad7e210d6b06475e71dc86b2c
lgb-this <matthias.manhart@gmx.net>
2024-03-03 10:44:31 +0100

    bug fixing

--- byd_bat/webif/__init__.py
+++ byd_bat/webif/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # vim: set encoding=utf-8 tabstop=4 softtabstop=4 shiftwidth=4 expandtab
 #########################################################################
-#  Copyright 2023       Matthias Manhart             smarthome@beathis.ch
+#  Copyright 2024       Matthias Manhart             smarthome@beathis.ch
 #########################################################################
 #  This file is part of SmartHomeNG.
 #  https://www.smarthomeNG.de
