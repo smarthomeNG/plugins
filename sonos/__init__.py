931057887ad81d64a38ce30629898750ffb46e77
aschwith <alex.schwithal@gmx.de>
2024-05-05 20:18:36 +0200

    sonos: update to SoCo 0.30.4 (refer to SoCo changelog for a full list of robustness fixes)

--- sonos/__init__.py
+++ sonos/__init__.py
@@ -2998,7 +2998,7 @@ class Sonos(SmartPlugin):
     """
     Main class of the Plugin. Does all plugin specific stuff
     """
-    PLUGIN_VERSION = "1.8.5"
+    PLUGIN_VERSION = "1.8.6"
 
     def __init__(self, sh):
         """Initializes the plugin."""
