a019ef20ccfab4d8504b01564fd74eba717ddc1f
Onkel Andy <onkelandy@hotmail.com>
2026-01-13 12:31:01 +0100

    gpio plugin: update copyright info

--- gpio/__init__.py
+++ gpio/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # vim: set encoding=utf-8 tabstop=4 softtabstop=4 shiftwidth=4 expandtab
 #########################################################################
-#  Copyright 2016 <Onkel Andy>                    <onkelandy@hotmail.com>
+#  Copyright 2026 <Onkel Andy>                    <onkelandy@hotmail.com>
 #########################################################################
 #  This file is part of SmartHomeNG.
 #
