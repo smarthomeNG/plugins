e9afcf20972f33d95ab80f1ff3749b0f52e2fe4d
Morg42 <43153739+Morg42@users.noreply.github.com>
2026-08-26 15:04:40 +0200

    database: bump version to 1.7.0, update sh_minversion

--- database/__init__.py
+++ database/__init__.py
@@ -71,7 +71,7 @@ class Database(SmartPlugin):
     """
 
     ALLOW_MULTIINSTANCE = True
-    PLUGIN_VERSION = '1.6.15'
+    PLUGIN_VERSION = '1.7.0'
 
     # SQL queries: {item} = item table name, {log} = log table name
     # time, item_id, val_str, val_num, val_bool, changed
